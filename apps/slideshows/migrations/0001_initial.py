a04cc31d7fb0eb4ec3672699d68d2c603b6b4c6a
Aidas Bendoraitis <bendoraitis@studio38.de>
2012-06-18 15:16:48 +0000

    * slideshows migrations updated

--- apps/slideshows/migrations/0001_initial.py
+++ apps/slideshows/migrations/0001_initial.py
@@ -12,21 +12,36 @@ class Migration(SchemaMigration):
         
         # Adding model 'Slideshow'
         db.create_table('slideshows_slideshow', south_cleaned_fields((
-            ('sysname', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=255, db_index=True)),
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('sysname', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=255, db_index=True)),
         )))
         db.send_create_signal('slideshows', ['Slideshow'])
 
         # Adding model 'Slide'
         db.create_table('slideshows_slide', south_cleaned_fields((
-            ('alt_en', self.gf('django.db.models.fields.CharField')(u'Alternative text', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=100, db_tablespace='', blank=True, unique=False, db_index=False)),
-            ('alt_de', self.gf('django.db.models.fields.CharField')(u'Alternative text', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=100, db_tablespace='', blank=True, unique=False, db_index=False)),
-            ('slideshow', self.gf('django.db.models.fields.related.ForeignKey')(default=0, to=orm['slideshows.Slideshow'])),
-            ('sort_order', self.gf('django.db.models.fields.IntegerField')(default=None)),
-            ('link', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
-            ('path', self.gf('filebrowser.fields.FileBrowseField')(max_length=255, blank=True)),
-            ('alt', self.gf('base_libs.models.fields.MultilingualCharField')(max_length=100, null=True, blank=True)),
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('slideshow', self.gf('django.db.models.fields.related.ForeignKey')(default=0, to=orm['slideshows.Slideshow'])),
+            ('path', self.gf('filebrowser.fields.FileBrowseField')(directory='slideshows/', max_length=255, blank=True)),
+            ('link', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
+            ('alt', self.gf('base_libs.models.fields.MultilingualCharField')(max_length=100, null=True, blank=True)),
+            ('title', self.gf('base_libs.models.fields.MultilingualCharField')(max_length=255, null=True, blank=True)),
+            ('title2', self.gf('base_libs.models.fields.MultilingualCharField')(max_length=255, null=True, blank=True)),
+            ('title3', self.gf('base_libs.models.fields.MultilingualCharField')(max_length=255, null=True, blank=True)),
+            ('subtitle', self.gf('base_libs.models.fields.MultilingualCharField')(max_length=255, null=True, blank=True)),
+            ('credits', self.gf('base_libs.models.fields.MultilingualCharField')(max_length=255, null=True, blank=True)),
+            ('sort_order', self.gf('django.db.models.fields.IntegerField')(default=None)),
+            ('subtitle_de', self.gf('django.db.models.fields.CharField')(u'Subtitle', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=255, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('subtitle_en', self.gf('django.db.models.fields.CharField')(u'Subtitle', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=255, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('title_de', self.gf('django.db.models.fields.CharField')(u'Title Line 1', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=255, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('title_en', self.gf('django.db.models.fields.CharField')(u'Title Line 1', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=255, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('credits_de', self.gf('django.db.models.fields.CharField')(u'Photo credits', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=255, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('credits_en', self.gf('django.db.models.fields.CharField')(u'Photo credits', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=255, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('title2_de', self.gf('django.db.models.fields.CharField')(u'Title Line 2', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=255, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('title2_en', self.gf('django.db.models.fields.CharField')(u'Title Line 2', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=255, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('title3_de', self.gf('django.db.models.fields.CharField')(u'Title Line 3', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=255, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('title3_en', self.gf('django.db.models.fields.CharField')(u'Title Line 3', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=255, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('alt_de', self.gf('django.db.models.fields.CharField')(u'Alternative text', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=100, db_tablespace='', blank=True, unique=False, db_index=False)),
+            ('alt_en', self.gf('django.db.models.fields.CharField')(u'Alternative text', null=False, primary_key=False, db_column=None, default='', editable=True, max_length=100, db_tablespace='', blank=True, unique=False, db_index=False)),
         )))
         db.send_create_signal('slideshows', ['Slide'])
     
@@ -42,18 +57,33 @@ class Migration(SchemaMigration):
     
     models = {
         'slideshows.slide': {
-            'Meta': {'object_name': 'Slide'},
+            'Meta': {'ordering': "['sort_order']", 'object_name': 'Slide'},
             'alt': ('base_libs.models.fields.MultilingualCharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'alt_de': ('django.db.models.fields.CharField', ["u'Alternative text'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '100', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
             'alt_en': ('django.db.models.fields.CharField', ["u'Alternative text'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '100', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
+            'credits': ('base_libs.models.fields.MultilingualCharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'credits_de': ('django.db.models.fields.CharField', ["u'Photo credits'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '255', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
+            'credits_en': ('django.db.models.fields.CharField', ["u'Photo credits'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '255', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'link': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'path': ('filebrowser.fields.FileBrowseField', [], {'max_length': '255', 'blank': 'True'}),
+            'path': ('filebrowser.fields.FileBrowseField', [], {'directory': "'slideshows/'", 'max_length': '255', 'blank': 'True'}),
             'slideshow': ('django.db.models.fields.related.ForeignKey', [], {'default': '0', 'to': "orm['slideshows.Slideshow']"}),
-            'sort_order': ('django.db.models.fields.IntegerField', [], {'default': 'None'})
+            'sort_order': ('django.db.models.fields.IntegerField', [], {'default': 'None'}),
+            'subtitle': ('base_libs.models.fields.MultilingualCharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'subtitle_de': ('django.db.models.fields.CharField', ["u'Subtitle'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '255', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
+            'subtitle_en': ('django.db.models.fields.CharField', ["u'Subtitle'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '255', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
+            'title': ('base_libs.models.fields.MultilingualCharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'title2': ('base_libs.models.fields.MultilingualCharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'title2_de': ('django.db.models.fields.CharField', ["u'Title Line 2'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '255', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
+            'title2_en': ('django.db.models.fields.CharField', ["u'Title Line 2'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '255', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
+            'title3': ('base_libs.models.fields.MultilingualCharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'title3_de': ('django.db.models.fields.CharField', ["u'Title Line 3'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '255', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
+            'title3_en': ('django.db.models.fields.CharField', ["u'Title Line 3'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '255', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
+            'title_de': ('django.db.models.fields.CharField', ["u'Title Line 1'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '255', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'}),
+            'title_en': ('django.db.models.fields.CharField', ["u'Title Line 1'"], {'null': 'False', 'primary_key': 'False', 'db_column': 'None', 'default': "''", 'editable': 'True', 'max_length': '255', 'db_tablespace': "''", 'blank': 'True', 'unique': 'False', 'db_index': 'False'})
         },
         'slideshows.slideshow': {
-            'Meta': {'object_name': 'Slideshow'},
+            'Meta': {'ordering': "['sysname']", 'object_name': 'Slideshow'},
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'sysname': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '255', 'db_index': 'True'})
         }
