96bebb5e1aa750c73a52fbf9ea022389822f9770
Aidas Bendoraitis <bendoraitis@studio38.de>
2012-06-18 18:34:50 +0000

    * rich text field for editorial content

--- apps/editorial/cms_plugins.py
+++ apps/editorial/cms_plugins.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import re
+
 from django.utils.translation import ugettext as _
 
 from cms.plugin_base import CMSPluginBase
@@ -11,6 +13,14 @@ class EditorialContentPlugin(CMSPluginBase):
     model = EditorialContent
     name = _("Editorial Content")
     render_template = "cms/plugins/editorial_content.html"
+    change_form_template = "cms/plugins/editorial_content_plugin_change_form.html"
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        # add .markupType to body_markup_type
+        field = super(EditorialContentPlugin, self).formfield_for_dbfield(db_field, **kwargs)
+        if re.search('markup_type$', db_field.name):
+            field.widget.attrs['class'] = "markupType"
+        return field
     
     def render(self, context, instance, placeholder):
         context.update({
