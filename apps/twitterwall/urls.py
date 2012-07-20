c85c9acbe45d92e67c80072b0a3a2f439432fa73
Aidas Bendoraitis <bendoraitis@studio38.de>
2012-07-20 17:50:14 +0000

    * loading the newest tweets dynamically

--- apps/twitterwall/urls.py
+++ apps/twitterwall/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('museumsportal.apps.twitterwall.views',
     url(r'^$', 'twitterwall', name='twitterwall_index'),
+    url(r'^tweets/$', 'load_tweets', name='load_tweets'),
 )
--- apps/twitterwall/views.py
+++ apps/twitterwall/views.py
@@ -12,10 +12,46 @@ def twitterwall(request):
         by_user=True,
         status="published",
         sites=current_site,
-        )[:50]
+        ).order_by("-creation_date")[5:50]
     tweets_from_search = Tweet.objects.filter(
         from_search=True,
         status="published",
         sites=current_site,
-        )[:50]
+        ).order_by("-creation_date")[5:50]
     return render(request, "twitterwall/index.html", locals())
+    
+def load_tweets(request):
+    params = {
+        'status': "published",
+        'sites': Site.objects.get_current(),
+        }
+    if request.REQUEST.get('by_user', False):
+        params['by_user'] = True
+    if request.REQUEST.get('from_search', False):
+        params['from_search'] = True
+    since_id = request.REQUEST.get('since_id', "")
+    if since_id:
+        try:
+            since = Tweet.objects.get(id=since_id)
+        except:
+            pass
+        else:
+            params['creation_date__gt'] = since.creation_date
+    before_id = request.REQUEST.get('before_id', "")
+    if before_id:
+        try:
+            before = Tweet.objects.get(id=before_id)
+        except:
+            pass
+        else:
+            params['creation_date__lt'] = before.creation_date
+    try:
+        count = int(request.REQUEST.get('count', "0"))
+    except:
+        count = 0
+    
+    tweets = Tweet.objects.filter(**params).order_by("-creation_date")
+    if count:
+        tweets = tweets[:count]
+        
+    return render(request, "twitterwall/tweets.html", {'tweets': tweets})
