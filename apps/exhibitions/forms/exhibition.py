63842dc4004958467658f0bc3a0748a14b584dc4
Aidas Bendoraitis <bendoraitis@studio38.de>
2012-12-20 23:03:16 +0100

    checkboxes for opened weekdays instead of closed weekdays at the multistep forms of museums and exhibitions.

--- apps/exhibitions/forms/exhibition.py
+++ apps/exhibitions/forms/exhibition.py
@@ -167,13 +167,13 @@ class OpeningForm(forms.Form):
             )        
 
 class SeasonForm(ModelForm):
-    mon_is_closed = forms.BooleanField(label=_("Mon"), required=False)
-    tue_is_closed = forms.BooleanField(label=_("Tue"), required=False)
-    wed_is_closed = forms.BooleanField(label=_("Wed"), required=False)
-    thu_is_closed = forms.BooleanField(label=_("Thu"), required=False)
-    fri_is_closed = forms.BooleanField(label=_("Fri"), required=False)
-    sat_is_closed = forms.BooleanField(label=_("Sat"), required=False)
-    sun_is_closed = forms.BooleanField(label=_("Sun"), required=False)
+    mon_is_open = forms.BooleanField(label=_("Mon"), required=False)
+    tue_is_open = forms.BooleanField(label=_("Tue"), required=False)
+    wed_is_open = forms.BooleanField(label=_("Wed"), required=False)
+    thu_is_open = forms.BooleanField(label=_("Thu"), required=False)
+    fri_is_open = forms.BooleanField(label=_("Fri"), required=False)
+    sat_is_open = forms.BooleanField(label=_("Sat"), required=False)
+    sun_is_open = forms.BooleanField(label=_("Sun"), required=False)
     
     class Meta:
         model = Season
@@ -222,49 +222,49 @@ class SeasonForm(ModelForm):
 
                 <tbody>
                     <tr>
-                        <th>"""), "mon_is_closed", layout.HTML("""</th>
+                        <th>"""), "mon_is_open", layout.HTML("""</th>
                         <td>"""), "mon_open", layout.HTML("""</td>
                         <td>"""), "mon_break_close", layout.HTML("""</td>
                         <td>"""), "mon_break_open", layout.HTML("""</td>
                         <td>"""), "mon_close", layout.HTML("""</td>
                     </tr>
                     <tr>
-                        <th>"""), "tue_is_closed", layout.HTML("""</th>
+                        <th>"""), "tue_is_open", layout.HTML("""</th>
                         <td>"""), "tue_open", layout.HTML("""</td>
                         <td>"""), "tue_break_close", layout.HTML("""</td>
                         <td>"""), "tue_break_open", layout.HTML("""</td>
                         <td>"""), "tue_close", layout.HTML("""</td>
                     </tr>
                     <tr>
-                        <th>"""), "wed_is_closed", layout.HTML("""</th>
+                        <th>"""), "wed_is_open", layout.HTML("""</th>
                         <td>"""), "wed_open", layout.HTML("""</td>
                         <td>"""), "wed_break_close", layout.HTML("""</td>
                         <td>"""), "wed_break_open", layout.HTML("""</td>
                         <td>"""), "wed_close", layout.HTML("""</td>
                     </tr>
                     <tr>
-                        <th>"""), "thu_is_closed", layout.HTML("""</th>
+                        <th>"""), "thu_is_open", layout.HTML("""</th>
                         <td>"""), "thu_open", layout.HTML("""</td>
                         <td>"""), "thu_break_close", layout.HTML("""</td>
                         <td>"""), "thu_break_open", layout.HTML("""</td>
                         <td>"""), "thu_close", layout.HTML("""</td>
                     </tr>
                     <tr>
-                        <th>"""), "fri_is_closed", layout.HTML("""</th>
+                        <th>"""), "fri_is_open", layout.HTML("""</th>
                         <td>"""), "fri_open", layout.HTML("""</td>
                         <td>"""), "fri_break_close", layout.HTML("""</td>
                         <td>"""), "fri_break_open", layout.HTML("""</td>
                         <td>"""), "fri_close", layout.HTML("""</td>
                     </tr>
                     <tr>
-                        <th>"""), "sat_is_closed", layout.HTML("""</th>
+                        <th>"""), "sat_is_open", layout.HTML("""</th>
                         <td>"""), "sat_open", layout.HTML("""</td>
                         <td>"""), "sat_break_close", layout.HTML("""</td>
                         <td>"""), "sat_break_open", layout.HTML("""</td>
                         <td>"""), "sat_close", layout.HTML("""</td>
                     </tr>
                     <tr>
-                        <th>"""), "sun_is_closed", layout.HTML("""</th>
+                        <th>"""), "sun_is_open", layout.HTML("""</th>
                         <td>"""), "sun_open", layout.HTML("""</td>
                         <td>"""), "sun_break_close", layout.HTML("""</td>
                         <td>"""), "sun_break_open", layout.HTML("""</td>
@@ -555,37 +555,37 @@ def load_data(instance=None):
             season_dict['mon_break_close'] = season.mon_break_close
             season_dict['mon_break_open'] = season.mon_break_open
             season_dict['mon_close'] = season.mon_close
-            season_dict['mon_is_closed'] = not season.mon_open
+            season_dict['mon_is_open'] = bool(season.mon_open)
             season_dict['tue_open'] = season.tue_open
             season_dict['tue_break_close'] = season.tue_break_close
             season_dict['tue_break_open'] = season.tue_break_open
             season_dict['tue_close'] = season.tue_close
-            season_dict['tue_is_closed'] = not season.tue_open
+            season_dict['tue_is_open'] = bool(season.tue_open)
             season_dict['wed_open'] = season.wed_open
             season_dict['wed_break_close'] = season.wed_break_close
             season_dict['wed_break_open'] = season.wed_break_open
             season_dict['wed_close'] = season.wed_close
-            season_dict['wed_is_closed'] = not season.wed_open
+            season_dict['wed_is_open'] = bool(season.wed_open)
             season_dict['thu_open'] = season.thu_open
             season_dict['thu_break_close'] = season.thu_break_close
             season_dict['thu_break_open'] = season.thu_break_open
             season_dict['thu_close'] = season.thu_close
-            season_dict['thu_is_closed'] = not season.thu_open
+            season_dict['thu_is_open'] = bool(season.thu_open)
             season_dict['fri_open'] = season.fri_open
             season_dict['fri_break_close'] = season.fri_break_close
             season_dict['fri_break_open'] = season.fri_break_open
             season_dict['fri_close'] = season.fri_close
-            season_dict['fri_is_closed'] = not season.fri_open
+            season_dict['fri_is_open'] = bool(season.fri_open)
             season_dict['sat_open'] = season.sat_open
             season_dict['sat_break_close'] = season.sat_break_close
             season_dict['sat_break_open'] = season.sat_break_open
             season_dict['sat_close'] = season.sat_close
-            season_dict['sat_is_closed'] = not season.sat_open
+            season_dict['sat_is_open'] = bool(season.sat_open)
             season_dict['sun_open'] = season.sun_open
             season_dict['sun_break_close'] = season.sun_break_close
             season_dict['sun_break_open'] = season.sun_break_open
             season_dict['sun_close'] = season.sun_close
-            season_dict['sun_is_closed'] = not season.sun_open
+            season_dict['sun_is_open'] = bool(season.sun_open)
             for lang_code, lang_name in FRONTEND_LANGUAGES:
                 season_dict['last_entry_%s' % lang_code] = getattr(season, 'last_entry_%s' % lang_code)
                 season_dict['exceptions_%s' % lang_code] = getattr(season, 'exceptions_%s' % lang_code)
@@ -647,37 +647,37 @@ def submit_step(current_step, form_steps, form_step_data, instance=None):
                 season_dict['mon_break_close'] = season.mon_break_close
                 season_dict['mon_break_open'] = season.mon_break_open
                 season_dict['mon_close'] = season.mon_close
-                season_dict['mon_is_closed'] = not season.mon_open
+                season_dict['mon_is_open'] = bool(season.mon_open)
                 season_dict['tue_open'] = season.tue_open
                 season_dict['tue_break_close'] = season.tue_break_close
                 season_dict['tue_break_open'] = season.tue_break_open
                 season_dict['tue_close'] = season.tue_close
-                season_dict['tue_is_closed'] = not season.tue_open
+                season_dict['tue_is_open'] = bool(season.tue_open)
                 season_dict['wed_open'] = season.wed_open
                 season_dict['wed_break_close'] = season.wed_break_close
                 season_dict['wed_break_open'] = season.wed_break_open
                 season_dict['wed_close'] = season.wed_close
-                season_dict['wed_is_closed'] = not season.wed_open
+                season_dict['wed_is_open'] = bool(season.wed_open)
                 season_dict['thu_open'] = season.thu_open
                 season_dict['thu_break_close'] = season.thu_break_close
                 season_dict['thu_break_open'] = season.thu_break_open
                 season_dict['thu_close'] = season.thu_close
-                season_dict['thu_is_closed'] = not season.thu_open
+                season_dict['thu_is_open'] = bool(season.thu_open)
                 season_dict['fri_open'] = season.fri_open
                 season_dict['fri_break_close'] = season.fri_break_close
                 season_dict['fri_break_open'] = season.fri_break_open
                 season_dict['fri_close'] = season.fri_close
-                season_dict['fri_is_closed'] = not season.fri_open
+                season_dict['fri_is_open'] = bool(season.fri_open)
                 season_dict['sat_open'] = season.sat_open
                 season_dict['sat_break_close'] = season.sat_break_close
                 season_dict['sat_break_open'] = season.sat_break_open
                 season_dict['sat_close'] = season.sat_close
-                season_dict['sat_is_closed'] = not season.sat_open
+                season_dict['sat_is_open'] = bool(season.sat_open)
                 season_dict['sun_open'] = season.sun_open
                 season_dict['sun_break_close'] = season.sun_break_close
                 season_dict['sun_break_open'] = season.sun_break_open
                 season_dict['sun_close'] = season.sun_close
-                season_dict['sun_is_closed'] = not season.sun_open
+                season_dict['sun_is_open'] = bool(season.sun_open)
                 for lang_code, lang_name in FRONTEND_LANGUAGES:
                     season_dict['last_entry_%s' % lang_code] = getattr(season, 'last_entry_%s' % lang_code)
                     season_dict['exceptions_%s' % lang_code] = getattr(season, 'exceptions_%s' % lang_code)
