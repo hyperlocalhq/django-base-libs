e7fc35f621fcdf1a43c799f03cfc48dc868ab1b9
Aidas Bendoraitis <bendoraitis@studio38.de>
2013-02-05 16:26:04 +0100

    js for gallery form put into a separate file

---
+++ site_static/site/js/gallery_form.js
@@ -0,0 +1,52 @@
+(function($, undefined) {
+    function reinit() {
+        $('#photos').each(function() {
+            var load_url = $(this).data("load-url");
+            $(this).load(load_url + ' #photos>*', function() {
+                $('#photos').sortable({
+                    placeholder: "ui-state-highlight",
+                    update: function(event, ui) {
+                        var tokens = []
+                        $('.img_and_desc', '#photos').each(function() {
+                            tokens[tokens.length] = $(this).data('token');
+                        });
+                        $.post(load_url, {ordering: tokens.join(',')});
+                    }
+                })
+                $('#photos').disableSelection().find('.edit_photo').click(function() {
+                    $('#edit_photo').load($(this).attr('href') + ' .content form', function() {
+                        $('#id_goto_next').val(location.href);
+                        $('#button-id-cancel').click(function() {
+                            $('#edit_photo').html("");
+                        });
+                    });
+                    return false;
+                });
+                $('#photos').find('.delete_photo').click(function() {
+                    $('#edit_photo').load($(this).attr('href') + ' .content form', function() {
+                        $('#id_goto_next').val(location.href);
+                        $('#button-id-cancel').click(function() {
+                            $('#edit_photo').html("");
+                        });
+                    });
+                    return false;
+                });
+                $('#photos').find('.crop_photo').each(function() {
+                    $(this).attr('href', $(this).attr('href').replace(/goto_next=.+$/gim, "goto_next=" + location.href));
+                });
+            });
+        });
+    }
+    $(function() {
+        reinit();
+        $('#add_photo').click(function() {
+            $('#edit_photo').load($(this).attr('href') + ' .content form', function() {
+                $('#id_goto_next').val(location.href);
+                $('#button-id-cancel').click(function() {
+                    $('#edit_photo').html("");
+                });
+            });
+            return false;
+        });
+    });
+})(jQuery);
