e04484b6736afa419d3251073941b2e7d0580f88
Thomas Helbig <mail@dergraph.com>
2013-03-14 18:29:56 +0100

    yes

--- site_static/site/js/sticky.js
+++ site_static/site/js/sticky.js
@@ -1,4 +1,4 @@
-var $stickyDiv = $(".sticky");
+var $stickyDiv = $("x.sticky");
 var $stickyHeight = 600;
 var $padding = 0;
 var $topOffset = 150;
@@ -9,19 +9,19 @@ function scrollSticky(){
         
         var aOffset = $stickyDiv.offset();
         var height = $stickyDiv.height();
-
+        
         if($(document).height() - $footerHeight - $padding < $(window).scrollTop() + $stickyHeight) {
             var $top = $(document).height() - $stickyHeight - $footerHeight - $padding;
             $stickyDiv.attr('style', 'position:absolute; top:'+$top+'px;');
-            // $('#container').css("margin-top",height);
+            // $('.sticky_compensation').css("margin-top",height);
 
         }else if($(window).scrollTop() + $padding > $topOffset) {
             $stickyDiv.attr('style', 'position:fixed; top:'+$padding+'px;');
-            // $('#container').css("margin-top",height);
+            // $('.sticky_compensation').css("margin-top",height);
 
         }else{
             $stickyDiv.attr('style', 'position:relative;');
-            // $('#container').css("margin-top",0);
+            // $('.sticky_compensation').css("margin-top",0);
         }
     }
 }
