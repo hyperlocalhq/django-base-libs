00ad9da6cfe266fced8583c2f44aa6422a673625
Aidas Bendoraitis <bendoraitis@studio38.de>
2013-10-01 20:11:46 +0200

    slideshow as a separate page for museums, events, and workshops

--- apps/museums/urls.py
+++ apps/museums/urls.py
@@ -4,9 +4,12 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('museumsportal.apps.museums.views',
     url(r'^$', 'museum_list', name='museum_list'),
+    url(r'^map/$', 'museum_list_map', name='museum_list_map'),
     url(r'^export-json-museums/$', 'export_json_museums', name='export_json_museums'),
     url(r'^add/$', 'add_museum', name='add_museum'),    
     url(r'^(?P<slug>[^/]+)/$', 'museum_detail', name='museum_detail'),    
+    url(r'^(?P<slug>[^/]+)/ajax/$', 'museum_detail_ajax', name='museum_detail_ajax'),
+    url(r'^(?P<slug>[^/]+)/slideshow/$', 'museum_detail_slideshow', name='museum_detail_slideshow'),
     url(r'^(?P<slug>[^/]+)/change/$', 'change_museum', name='change_museum'),
     # gallery
     url(r'^(?P<slug>[^/]+)/gallery/$', 'gallery_overview', name='museum_gallery_overview'),    
