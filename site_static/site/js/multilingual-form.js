e53c5ffff52f018637cc5b2ffe75a8a4381d07e0
Aidas Bendoraitis <bendoraitis@studio38.de>
2013-12-19 14:32:43 +0100

    multilingual forms

--- site_static/site/js/multilingual-form.js
+++ site_static/site/js/multilingual-form.js
@@ -1,13 +1,23 @@
-$(function() {
+$(window).load(function() {
     function activate(language) {
         $('.multilingual').addClass('hide');
-        $('.lang-' + current_language).removeClass('hide');
+        $('.lang-' + language).removeClass('hide');
     }
-    var current_language = window.settings.lang;
-    activate(current_language);
+
+    /* show the language-specific fields only for the current language */
+    activate(window.settings.lang);
 
     var $form_language = $('#form_language');
-    $('a',$form_language).click(function() {
+
+    /* set a badge with an exclamation mark to the language which language-specific fields have errors */
+    $.each(window.settings.frontend_languages, function(lang_code, lang_name) {
+        if ($('.lang-' + lang_code + ' .has-error').length) {
+            $('li[data-lang="' + lang_code + '"] a', $form_language).append(' <span class="badge">!</span>');
+        }
+    });
+
+    /* handle language switching */
+    $('a', $form_language).click(function() {
         $('.active', $form_language).removeClass('active');
         var $item = $(this).closest('li').addClass('active');
         var language = $item.data('lang');
--- templates/museumsportal/settings.js
+++ templates/museumsportal/settings.js
@@ -3,3 +3,7 @@
 {% block document_domain %}
 //"{{ cookie_domain }}";
 {% endblock %}
+
+{% block extra_window_settings %}
+window.settings.frontend_languages = { {% for lang in FRONTEND_LANGUAGES %}'{{ lang.0|escapejs }}': '{{ lang.1|escapejs }}'{% if not forloop.last %},{% endif %}{% endfor %} };
+{% endblock %}
