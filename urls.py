55bd1c11cfedb996b5db4411b24ee0e952cf4b7c
Aidas Bendoraitis <bendoraitis@studio38.de>
2014-04-02 19:58:45 +0200

    cosmetic change in the museumsportal urls

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic import TemplateView
