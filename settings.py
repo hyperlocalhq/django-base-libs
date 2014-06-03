08814b7dc7bfb249f44b499c0eff1eb98bb94e03
Aidas Bendoraitis <bendoraitis@studio38.de>
2014-06-03 15:49:29 +0200

    missing template setting

--- settings.py
+++ settings.py
@@ -555,6 +555,7 @@ CMS_TEMPLATES = [
     ('cms/magazine_second_featured.html', gettext(u'Magazine – Second - Featured')), # previous: series_with_featured.html
     ('cms/magazine_third.html', gettext(u'Magazine - Third')),
     
+    ('cms/shop_overview.html', gettext(u'Shop – Overview')),
     ('cms/shop_first.html', gettext(u'Shop – First')),
 
     ('cms/dashboard_default.html', gettext('Dashboard Default')),
