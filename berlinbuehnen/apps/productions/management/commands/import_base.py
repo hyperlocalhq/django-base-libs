2bbc882ccb961edc2c99ac290e3d4f244b89ef83
Aidas Bendoraitis <bendoraitis@studio38.de>
2015-01-14 22:28:32 +0100

    Import from heimat.de-based websites

--- berlinbuehnen/apps/productions/management/commands/import_base.py
+++ berlinbuehnen/apps/productions/management/commands/import_base.py
@@ -3,6 +3,7 @@
 import re
 from dateutil.parser import parse as parse_datetime
 import requests
+import csv
 
 from django.db import models
 
@@ -12,8 +13,10 @@ from django.utils.text import slugify
 from base_libs.utils.misc import get_unique_value
 
 from berlinbuehnen.apps.productions.models import ProductionCategory
+from berlinbuehnen.apps.productions.models import ProductionCharacteristics
 from berlinbuehnen.apps.productions.models import Production
 from berlinbuehnen.apps.productions.models import ProductionImage
+from berlinbuehnen.apps.productions.models import EventCharacteristics
 from berlinbuehnen.apps.productions.models import Event
 from berlinbuehnen.apps.productions.models import EventImage
 from berlinbuehnen.apps.people.models import Person
@@ -22,10 +25,159 @@ from berlinbuehnen.apps.sponsors.models import Sponsor
 SILENT, NORMAL, VERBOSE, VERY_VERBOSE = 0, 1, 2, 3
 
 
+class CultureBaseLocation(object):
+    def __init__(self, id, title, street_address, postal_code, city, *args, **kwargs):
+        self.id = int(id)
+        self.title = force_unicode(title)
+        self.street_address = force_unicode(street_address)
+        self.postal_code = force_unicode(postal_code)
+        self.city = force_unicode(city)
+
+    def __unicode__(self):
+        return self.title
+
+    def __repr__(self):
+        return smart_str(u"<CultureBaseLocation: %s>" % self.title)
+
+
 class ImportFromHeimatBase(object):
     """ Base interface for importing productions and events from different websites
     according to this XML schema: http://cb.heimat.de/interface/schema/interfaceformat.xsd
     """
+    LOCATIONS = {}
+    service = None
+    CATEGORY_MAPPER = {
+        7002: 74,  # Ausstellung
+        6999: 25,  # Ballett
+        6995: 35,  # Blues
+        6988: 36,  # Chanson
+        7009: 56,  # Comedy
+        6994: 37,  # Country
+        7003: 68,  # Diskussion
+        7001: 38,  # Elektro
+        7010: 76,  # Film
+        6993: 39,  # Folk
+        6986: 77,  # Fotografie
+        6992: 40,  # Funk
+        6987: 78,  # Fuhrung
+        6991: 41,  # HipHop
+        6990: 42,  # Jazz
+        7008: 57,  # Kabarett
+        7022: 8,  # Kinder/Jugend
+        7013: 43,  # Klassik
+        7017: 14,  # Komödie
+        7019: 69,  # Konferenz
+        6998: 17,  # Konzertante Vorstellung
+        7020: 15,  # Lesung
+        7000: 14,  # Liederabend
+        7014: 18,  # Musical
+        7018: 79,  # Neue Medien
+        6996: 45,  # Neue Musik
+        7024: 20,  # Oper
+        7015: 22,  # Operette
+        7012: 80,  # Party
+        7016: 32,  # Performance
+        7006: 46,  # Pop
+        7007: 66,  # Puppentheater
+        7026: 52,  # Revue
+        7005: 47,  # Rock
+        7028: 16,  # Schauspiel
+        7025: 53,  # Show
+        7023: 5,  # Sonstige Musik
+        6989: 48,  # Soul
+        7011: 49,  # Special
+        6997: 27,  # Tanztheater
+        7027: 54,  # Variete
+        7021: 70,  # Vortrag
+        7004: 82,  # Workshop
+    }
+    PRODUCTION_CHARACTERISTICS_MAPPER = {
+        1: '',  # Premiere
+        2: 'wiederaufname',  # Wiederaufnahme
+        3: '',  # Vorauffuhrung
+        5: '',  # Publikumsgespräch
+        6: 'gastspiel',  # Gastspiel
+        11: '',  # öffentliche Probe
+        17: '',  # Deutsche Erstauffuhrung
+        21: 'repertoire',  # Repertoire
+        22: '',  # zum letzten Mal
+        23: 'urauffuehrung',  # Urauffuhrung
+        24: '',  # Familienvorstellung
+        25: '',  # Kindervorstellung
+        27: '',  # Einfuhrung
+        28: '',  # zum letzten Mal in dieser Spielzeit
+        30: '',  # Deutschsprachige Erstauffuhrung
+        31: 'on-tour',  # On Tour
+        33: '',  # B-Premiere
+        34: '',  # Matinee
+        35: '',  # Schulervorstellung
+        36: '',  # Volkstheater
+    }
+    EVENT_CHARACTERISTICS_MAPPER = {
+        1: 'premiere',  # Premiere
+        2: '',  # Wiederaufnahme
+        3: 'vorauffuehrung',  # Vorauffuhrung
+        5: '',  # Publikumsgespräch
+        6: '',  # Gastspiel
+        11: '',  # öffentliche Probe
+        17: 'deutsche-erstauffuehrung',  # Deutsche Erstauffuhrung
+        21: '',  # Repertoire
+        22: 'zum-letzten-mal',  # zum letzten Mal
+        23: '',  # Urauffuhrung
+        24: '',  # Familienvorstellung
+        25: '',  # Kindervorstellung
+        27: '',  # Einfuhrung
+        28: 'zum-letzten-mal-dieser-spielzeit',  # zum letzten Mal in dieser Spielzeit
+        30: 'deutschsprachige-erstauffuehrung',  # Deutschsprachige Erstauffuhrung
+        31: '',  # On Tour
+        33: 'berliner-premiere',  # B-Premiere
+        34: '',  # Matinee
+        35: '',  # Schulervorstellung
+        36: '',  # Volkstheater
+    }
+    ROLE_ID_MAPPER = {
+        1: (u'Regie', u'Director'),
+        4: (u'Dramaturgie', u'Dramaturgy'),
+        6: (u'Ausstatter/-in', u'Designer'),
+        8: (u'Schauspieler/-in', u'Actor'),
+        9: (u'Sänger/-in', u'Singer'),
+        10: (u'Solist/-in', u'Solist'),
+        11: (u'Tänzer/-in', u'Dancer'),
+        12: (u'Dirigent/-in', u'Director'),
+        48: (u'Requisite', u'Requisite'),
+        15: (u'Künstlerische Leitung', u'Artistic director'),
+        17: (u'Buhnenbildner/-in', u'Scene designer'),
+        18: (u'Kostüme', u'Costumes'),
+        20: (u'Choreografie', u'Choreography'),
+        21: (u'Licht', u'Light'),
+        22: (u'Einstudierung', u'Production'),
+        23: (u'Regieassistenz', u'Assistant director'),
+        25: (u'Musik', u'Music'),
+        46: (u'Text', u'Text'),
+        29: (u'Video', u'Video'),
+        30: (u'Pyrotechnik', u'Pyrotechnics'),
+        33: (u'Souffleur/Souffleuse', u'Prompter'),
+        34: (u'Künstler/-in', u'Performer'),
+        47: (u'Inspizient/-in', u'Stage caller'),
+        36: (u'Moderator/-in', u'Moderator'),
+        37: (u'Referent/-in', u'Referent'),
+        38: (u'Diskussionsteilnehmer/-in', u'Discussant'),
+        39: (u'Rezitation', u'Recitation'),
+        40: (u'Orchester', u'Orchestra'),
+        41: (u'Chor', u'Choir'),
+        43: (u'Statisterie', u'Extra'),
+        45: (u'Ensemble', u'Ensemble'),
+    }
+
+    def load_and_parse_locations(self):
+        response = requests.get("http://web2.heimat.de/cb-out/exports/address/address_id.php?city=berlin")
+        if response.status_code != 200:
+            return
+        reader = csv.reader(response.content.splitlines(), delimiter=";")
+        for row in reader:
+            if row[0] == "locationId":  # skip the first row
+                continue
+            self.LOCATIONS[int(row[0])] = CultureBaseLocation(*row)
 
     def get_child_text(self, node, tag, **attrs):
         """
@@ -45,18 +197,22 @@ class ImportFromHeimatBase(object):
                 if child_node.get(name) != val:
                     all_attributes_match = False
                     break
-            if all_attributes_match:
+            if all_attributes_match and child_node.text:
                 return force_unicode(child_node.text)
         return u""
 
     def create_or_update_location(self, venue_node):
         from berlinbuehnen.apps.locations.models import Location
         ObjectMapper = models.get_model("external_services", "ObjectMapper")
-        city_suffix = re.compile(r' \[[^\]]+\]')
 
-        return None, False # TODO: finalize
-        # TODO: decide whether to leave title handling as external id
-        external_id = venue_node.get('Id') or self.get_child_text(venue_node, 'Name')
+        if not self.LOCATIONS:
+            self.load_and_parse_locations()
+
+        if venue_node.get('isId') == "1":
+            external_id = int(venue_node.text)
+        else:  # unknown location id
+            return None, False
+
         mapper = None
         try:
             # get exhibition from saved mapper
@@ -75,16 +231,15 @@ class ImportFromHeimatBase(object):
                 # don't import it again
                 return None, False
 
-        location.title = self.get_child_text(venue_node, 'Name')
-        lat = self.get_child_text(venue_node, 'Latitude')
-        if lat:
-            location.latitude = float(lat)
-        lng = self.get_child_text(venue_node, 'Longitude')
-        if lng:
-            location.longitude = float(lng)
-        location.street_address = self.get_child_text(venue_node, 'Street')
-        location.postal_code = self.get_child_text(venue_node, 'ZipCode')
-        location.city = city_suffix.sub("", self.get_child_text(venue_node, 'City') or "")
+        if external_id not in self.LOCATIONS:  # location not found in Berlin
+            return None, False
+
+        culturbase_location = self.LOCATIONS[external_id]
+
+        location.title = culturbase_location.title
+        location.street_address = culturbase_location.street_address
+        location.postal_code = culturbase_location.postal_code
+        location.city = "Berlin"
         location.save()
 
         if not mapper:
@@ -176,9 +331,9 @@ class ImportFromHeimatBase(object):
                             field_name="path",
                             subpath="productions/%s/gallery/" % prod.slug,
                         )
-                        if picture_node.get('publishType') == "0":  # TODO: clarify publishType statuses for pictures
+                        if picture_node.get('publishType') == "1":
                             mf.copyright_restrictions = "general_use"
-                        else:
+                        elif picture_node.get('publishType') == "3":
                             mf.copyright_restrictions = "protected"
                         mf.save()
                         try:
@@ -200,10 +355,17 @@ class ImportFromHeimatBase(object):
                 if internal_cat_id:
                     prod.categories.add(ProductionCategory.objects.get(pk=internal_cat_id))
 
+            for status_id_node in prod_node.findall('statusId'):
+                internal_ch_slug = self.PRODUCTION_CHARACTERISTICS_MAPPER.get(int(status_id_node.text), None)
+                if internal_ch_slug:
+                    prod.characteristics.add(ProductionCharacteristics.objects.get(slug=internal_ch_slug))
+
             if not prod.productioninvolvement_set.count():
                 for person_node in prod_node.findall('person'):
                     role_de = self.get_child_text(person_node, 'mediaText/text', languageId="1")
                     role_en = self.get_child_text(person_node, 'mediaText/text', languageId="2")
+                    if not role_de and int(person_node.get('roleId')) in self.ROLE_ID_MAPPER:
+                        role_de, role_en = self.ROLE_ID_MAPPER[int(person_node.get('roleId'))]
                     for person_name in person_node.get('personFreetext').split(", "):
                         first_name, last_name = person_name.rsplit(" ", 1)
                         p, created = Person.objects.get_or_create(
@@ -327,9 +489,9 @@ class ImportFromHeimatBase(object):
                                 field_name="path",
                                 subpath="productions/%s/events/%s/gallery/" % (prod.slug, event.pk),
                             )
-                            if picture_node.get('publishType') == "0":
+                            if picture_node.get('publishType') == "1":
                                 mf.copyright_restrictions = "general_use"
-                            else:
+                            elif picture_node.get('publishType') == "3":
                                 mf.copyright_restrictions = "protected"
                             mf.save()
                             try:
@@ -353,10 +515,17 @@ class ImportFromHeimatBase(object):
                         event.play_locations.clear()
                         event.play_locations.add(location)
 
+                for status_id_node in event_node.findall('statusId'):
+                    internal_ch_slug = self.EVENT_CHARACTERISTICS_MAPPER.get(int(status_id_node.text), None)
+                    if internal_ch_slug:
+                        event.characteristics.add(EventCharacteristics.objects.get(slug=internal_ch_slug))
+
                 if not event.eventinvolvement_set.count():
                     for person_node in event_node.findall('person'):
                         role_de = self.get_child_text(person_node, 'mediaText/text', languageId="1")
                         role_en = self.get_child_text(person_node, 'mediaText/text', languageId="2")
+                        if not role_de and int(person_node.get('roleId')) in self.ROLE_ID_MAPPER:
+                            role_de, role_en = self.ROLE_ID_MAPPER[int(person_node.get('roleId'))]
                         for person_name in person_node.get('personFreetext').split(", "):
                             first_name, last_name = person_name.rsplit(" ", 1)
                             p, created = Person.objects.get_or_create(
