c51943b94829be54b1a2ecc51151695de999c7bb
Aidas Bendoraitis <bendoraitis@studio38.de>
2015-02-17 13:25:01 +0100

    Production Basic Info Form: play_stages show stages dependent on the chosen play_locations

--- berlinbuehnen/apps/locations/autocomplete_light_registry.py
+++ berlinbuehnen/apps/locations/autocomplete_light_registry.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 -*-
 
+from django.utils.translation import ugettext_lazy as _
+
 import autocomplete_light
-from models import Location
+from models import Location, Stage
 
 # This will generate a PersonAutocomplete class
 autocomplete_light.register(Location,
@@ -9,7 +11,7 @@ autocomplete_light.register(Location,
     search_fields=['title_de', 'title_en', 'subtitle_de', 'subtitle_en'],
     attrs={
         # This will set the input placeholder attribute:
-        'placeholder': 'Start typing to enter location title?',
+        'placeholder': _('Start typing to enter location title'),
         # This will set the yourlabs.Autocomplete.minimumCharacters
         # options, the naming conversion is handled by jQuery
         'data-autocomplete-minimum-characters': 1,
@@ -24,3 +26,31 @@ autocomplete_light.register(Location,
         'class': 'modern-style',
     },
 )
+
+
+class AutocompleteStage(autocomplete_light.AutocompleteModelBase):
+    autocomplete_js_attributes = attrs = {
+        'placeholder': _('Start typing to enter stage title'),
+        'data-autocomplete-minimum-characters': 0,
+    }
+    widget_attrs = {
+        'data-widget-maximum-values': 5,
+        # Enable modern-style widget !
+        'class': 'modern-style',
+    }
+
+    def choices_for_request(self):
+        q = self.request.GET.get('q', '')
+        try:
+            location_ids = [int(location_id) for location_id in self.request.GET.get('location_ids', "").split(',')]
+        except ValueError:
+            location_ids = []
+
+        choices = self.choices.all()
+        if q:
+            choices = choices.filter(title__icontains=q)
+        choices = choices.filter(location__pk__in=location_ids)
+
+        return self.order_choices(choices)[0:self.limit_choices]
+
+autocomplete_light.register(Stage, AutocompleteStage)
