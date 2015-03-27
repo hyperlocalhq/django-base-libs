001cc848a33f8bfdeededdc407d7de5b936b1b0d
Daniel Lehmann <dlehmann@dreammedia.info>
2015-03-27 18:23:51 +0100

    gallery functionality update

--- berlinbuehnen/site_static/site/js/gallery.js
+++ berlinbuehnen/site_static/site/js/gallery.js
@@ -22,8 +22,14 @@
         me.margin_vertical = 20;
         
         me.$main = $main;
-        me.$items = $('.gallery-item', $main);   
-        me.player = new Array();
+        me.$items = $('.gallery-item', $main);  
+        me.player = new Array(); 
+        me.images = new Array();
+        
+        me.columns = 0;
+        me.image_loaded = false;
+        me.$active_image = null;
+        me.active_image_index = 0;
         
         
         
@@ -51,16 +57,34 @@
             me.player.push($player);
         });
         
-        $('.image', $main).each(function() {
+        $('.image', $main).each(function(index, element) {
             
             var $this = $(this);
-            var $image = $($this.children()[0]);
+            var $image = $('.gallery-image', $this);
+            var $text = $('.gallery-image-text', $this);
+            
+            $text.detach();
+            
+            $image.data("index", index);
+            $image.data("text", $text);
+            me.images.push($image);
+            
+            $image.click(function() {me.openLayer($(this));});
             $image.load(function() {me.onImageLoaded();});
             
         });
         
-        me.arrange();
-        $(window).resize(function() {me.arrange();});
+        var $layer = $('#gallery-layer');
+        if ($layer.length) {
+            me.$layer = $layer;
+        } else {
+            me.$layer = $('<div id="gallery-layer"><div id="gallery-layer-close"></div><div id="gallery-layer-prev"></div><div id="gallery-layer-next"></div></div>');
+            $('body').append(me.$layer);
+            $('#gallery-layer-close').click(function() {me.closeLayer();});
+        }
+        
+        me.onResize();
+        $(window).resize(function() {me.onResize();});
     }
     
     /**
@@ -74,11 +98,13 @@
         var width = me.$main.width();
         var columns_width = me.$columns.width();
         
-        var colums = 0;
+        var columns = 0;
         
-        if (columns_width > width * 0.8) colums = 1;
-        else if (columns_width > width * 0.43) colums = 2;
-        else colums = 3;
+        if (columns_width > width * 0.8) columns = 1;
+        else if (columns_width > width * 0.43) columns = 2;
+        else columns = 3;
+        
+        me.columns = columns;
         
         
         me.$items.css('display', 'block');
@@ -94,13 +120,13 @@
                 
                 $item.css('top', heights[0]+'px');
                 heights[0] += height;
-                if (colums != 1) heights[1] += height;
+                if (columns != 1) heights[1] += height;
                 
             } else {
                 
                 column = 0;
                 min_height = heights[0];
-                for (i=1; i<colums; i++) {
+                for (i=1; i<columns; i++) {
                     if (min_height > heights[i]) {
                         min_height = heights[i];
                         column = i;
@@ -110,7 +136,7 @@
                 $item.css('top', heights[column]+'px');
                 heights[column] += height;
                 
-                var left = 100/colums * column;
+                var left = 100/columns * column;
                 $item.css('left', left+'%');
             }
             
@@ -118,7 +144,7 @@
         
         
         var max_height = heights[0];
-        for (i=1; i<colums; i++) {
+        for (i=1; i<columns; i++) {
             if (max_height < heights[i]) max_height = heights[i];
         }
         
@@ -152,6 +178,153 @@
         me.arrange();
     }
     
+    /**
+     * Opens the layer.
+     *
+     * @param   $image   the jQuery image object which got clicked
+     */
+    Gallery.prototype.openLayer = function($image) {
+        
+        if (this.me) var me = this.me;
+        
+        me.resizedLayer();        
+        
+        $('#gallery-layer-prev').off();
+        $('#gallery-layer-next').off();
+        $('#gallery-layer-prev').click(function() {me.prevImage();});
+        $('#gallery-layer-next').click(function() {me.nextImage();});
+        
+        $('body').css('overflow', 'hidden');
+        me.$layer.css('display', 'block');
+        
+        me.showImage($image);
+        
+    }
+    
+    /**
+     * Closes the layer.
+     */
+    Gallery.prototype.closeLayer = function() {
+        
+        if (this.me) var me = this.me;
+        
+        me.$layer.css('display', 'none');
+        $('body').css('overflow', 'auto');
+           
+        me.$active_image.remove();
+        me.$active_image = null;
+    }
+    
+    /**
+     * Shows the next image.
+     */
+    Gallery.prototype.nextImage = function() {
+        
+        if (this.me) var me = this.me;
+        
+        me.active_image_index++;
+        if (me.active_image_index >= me.images.length) me.active_image_index = 0;
+        
+        me.showImage(me.images[me.active_image_index]);
+    }
+    
+    /**
+     * Shows the previews image.
+     */
+    Gallery.prototype.prevImage  = function() {
+        
+        if (this.me) var me = this.me;
+        
+        me.active_image_index--;
+        if (me.active_image_index < 0) me.active_image_index = me.images.length-1;
+        
+        me.showImage(me.images[me.active_image_index]);
+    }
+    
+    /**
+     * Shows the image.
+     *
+     * @param   $image   the jQuery image object to be shown
+     */
+    Gallery.prototype.showImage = function($image) {
+        
+        if (this.me) var me = this.me;
+        
+        if (me.$active_image) {
+            /*me.$active_image.animate({opacity: 0}, 400, function() {
+                this.remove();   
+            });*/
+            me.$active_image.remove();
+        }
+        
+        me.active_image_index = $image.data("index");
+        
+        var new_image = new Image();
+        var $new_image = $(new_image);
+        me.image_loaded = false;
+        $new_image.load(function() {me.image_loaded = true; me.resizedLayer();});
+        
+        var $image_wrapper = $('<div class="gallery-image-wrapper"/>');
+        $image_wrapper.prepend($new_image);
+        
+        var $image_text = $image.data("text");
+        $image_wrapper.append($image_text);
+        
+        me.$layer.prepend($image_wrapper);
+        
+        me.$active_image = $image_wrapper;
+        new_image.src = $image.attr("data-image-big");
+        me.resizedLayer();
+    }
+    
+    /**
+     * Adjusts the layer on resize.
+     */
+    Gallery.prototype.resizedLayer = function() {
+        
+        if (this.me) var me = this.me;
+        
+        me.$layer.removeClass("medium").removeClass("small");
+        if (me.columns == 2) me.$layer.addClass("medium");
+        else if (me.columns == 1) me.$layer.addClass("small");
+        
+        if (me.$active_image && me.image_loaded) {
+            
+            var min_top_padding = (me.columns == 1) ? 0 : 20;
+            var min_bottom_padding = 25;
+            
+            var $image = $('img', me.$active_image);
+            var $text = $('.gallery-image-text', me.$active_image);
+        
+            $image.css("padding-top", min_top_padding+"px");
+            $image.css("padding-bottom", min_bottom_padding+"px");
+            $text.width($image.width());
+            
+            var text_height = $text.height();
+            $image.css("padding-bottom", (text_height + min_bottom_padding)+"px");
+            var image_height = $image.height();
+            
+            var top_padding = Math.round((me.$active_image.height() - (image_height + text_height + min_top_padding + min_bottom_padding)) / 2) + min_top_padding;
+            $image.css("padding-top", top_padding+"px");
+            
+            $text.width($image.width());
+            $text.css("top", (image_height + top_padding) + "px");
+            $text.css("left", $image.position().left + "px");
+            
+            me.$active_image.animate({opacity: 1}, 400);
+        }
+    }
+    
+    /**
+     * The window got resized.
+     */
+    Gallery.prototype.onResize = function() {
+        
+        if (this.me) var me = this.me;
+        
+        me.arrange();
+        if (me.$layer.css('display') == "block") me.resizedLayer();
+    }
     
     function init() {
         
