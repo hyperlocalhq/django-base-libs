101bc1a695d25f22d50d66a5ea17bd8fc012c590
Aidas Bendoraitis <bendoraitis@studio38.de>
2015-04-09 00:18:19 +0200

    cosmetic change

--- berlinbuehnen/apps/productions/templatetags/production_tags.py
+++ berlinbuehnen/apps/productions/templatetags/production_tags.py
@@ -19,7 +19,7 @@ def other_productions(context, current_event=False, current_location=False, amou
     other_production_set = Production.objects.filter(
         models.Q(in_program_of__in=locations) | models.Q(play_locations__in=locations),
         show_among_others=True,
-        #status="published",
+        #status="published",  # TODO: uncomment
     )
     if current_event:
         other_production_set = other_production_set.exclude(id=current_event.production.id)
