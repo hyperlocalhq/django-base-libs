e6f22e7238cd300a2848dd2c07a0ab5441eb76a9
Aidas Bendoraitis <bendoraitis@studio38.de>
2015-06-02 20:13:43 +0200

    template tags: modify_query, add_to_query, remove_from_query

--- base_libs/templatetags/base_tags.py
+++ base_libs/templatetags/base_tags.py
@@ -2,6 +2,7 @@
 import datetime
 import random
 import re
+import urllib
 from htmlentitydefs import name2codepoint
 from django.db import models
 from django import template
@@ -14,6 +15,7 @@ from django.template.loader import select_template
 from django.utils.safestring import mark_safe
 from django.utils.text import normalize_newlines
 from django.template.defaultfilters import stringfilter
+from django.utils.encoding import force_str
 
 from base_libs.utils.loader import select_template_for_object
 from base_libs.utils.user import get_user_title
@@ -574,7 +576,80 @@ class AppendGetNode(template.Node):
 @easy_tag
 def append_to_get(_tag_name, dict, no_path=False):
     return AppendGetNode(dict, no_path)
-    
+
+
+@register.simple_tag(takes_context=True)
+def modify_query(context, *params_to_remove, **params_to_change):
+    """ Renders a link with modified current query parameters """
+    query_params = []
+    for key, value_list in context['request'].GET._iterlists():
+        if not key in params_to_remove:
+            # don't add key-value pairs for params to change
+            if key in params_to_change:
+                query_params.append((key, params_to_change[key]))
+                params_to_change.pop(key)
+            else:
+                # leave existing parameters as they were if not mentioned in the params_to_change
+                for value in value_list:
+                    query_params.append((key, value))
+    # attach new params
+    for key, value in params_to_change.items():
+        query_params.append((key, value))
+    query_string = u""
+    if len(query_params):
+        query_string += u"?%s" % urllib.urlencode([
+            (key, force_str(value)) for (key, value) in query_params if value
+        ]).replace("&", "&amp;")
+    return query_string
+
+
+@register.simple_tag(takes_context=True)
+def add_to_query(context, *params_to_remove, **params_to_add):
+    """ Renders a link with modified current query parameters """
+    query_params = []
+    # go through current query params..
+    for key, value_list in context['request'].GET._iterlists():
+        if not key in params_to_remove:
+            # don't add key-value pairs which already exist in the query
+            if key in params_to_add and unicode(params_to_add[key]) in value_list:
+                params_to_add.pop(key)
+            for value in value_list:
+                query_params.append((key, value))
+    # add the rest key-value pairs
+    for key, value in params_to_add.items():
+        query_params.append((key, value))
+    # empty values will be removed
+    query_string = u""
+    if len(query_params):
+        query_string += u"?%s" % urllib.urlencode([
+            (key, force_str(value)) for (key, value) in query_params if value
+        ]).replace("&", "&amp;")
+    return query_string
+
+
+@register.simple_tag(takes_context=True)
+def remove_from_query(context, *args, **kwargs):
+    """ Renders a link with modified current query parameters """
+    query_params = []
+    # go through current query params..
+    for key, value_list in context['request'].GET._iterlists():
+        # skip keys mentioned in the args
+        if not key in args:
+            for value in value_list:
+                # skip key-value pairs mentioned in kwargs
+                if not (key in kwargs and value == unicode(kwargs[key])):
+                    query_params.append((key, value))
+    # empty values will be removed
+    query_string = u""
+    if len(query_params):
+        query_string += u"?%s" % urllib.urlencode([
+            (key, force_str(value)) for (key, value) in query_params if value
+        ]).replace("&", "&amp;")
+    return query_string
+
+
+
+
 class IncludeNode(template.Node):
     def __init__(self, template_name):
         self.template_name = template_name
