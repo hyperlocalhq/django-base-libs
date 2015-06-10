512cb51666e0f8a8cf6d8a3d134c526202a5ef8e
Tiago Henriques <henriques@studio38.de>
2015-06-10 15:57:44 +0200

    use base url for functional test

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,7 @@ class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
+        self.base_url = 'http://localhost:8000'
 
     def tearDown(self):
         self.browser.quit()
@@ -13,7 +14,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_open_homepage(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.base_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('Creative City Berlin', self.browser.title)
