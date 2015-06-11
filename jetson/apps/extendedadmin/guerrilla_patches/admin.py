413382ffe63bbf0aad2becd638f623fb703bdd93
Tiago Henriques <henriques@studio38.de>
2015-06-11 18:53:07 +0200

    remove RelatedFilterSpec patch

--- jetson/apps/extendedadmin/guerrilla_patches/admin.py
+++ jetson/apps/extendedadmin/guerrilla_patches/admin.py
@@ -4,7 +4,6 @@ from django import http, template
 from django import forms
 from django.db import models
 from django.contrib.admin.sites import AdminSite
-from django.contrib.admin.filterspecs import RelatedFilterSpec
 from django.contrib.admin.helpers import AdminField
 from django.shortcuts import render_to_response
 from django.utils.safestring import mark_safe
@@ -40,34 +39,6 @@ def patch_admin():
         self.is_markup_type = re.search('markup_type$', field) is not None
     AdminField.__init__ = new_admin_field_init
     
-    def new_init(self, f, request, params, model, model_admin, field_path=None):
-        """
-        patched RelatedFilterSpec.__init__:
-        this patch causes filters on foreign key fields in the admin not to show 
-        all entries but only those ones which actually are used.
-        """
-        super(RelatedFilterSpec, self).__init__(f, request, params, model, model_admin, field_path=field_path)
-        other_model = get_model_from_relation(f)
-        if isinstance(f, (models.ManyToManyField,
-                          models.related.RelatedObject)):
-            # no direct field on this model, get name from other model
-            self.lookup_title = other_model._meta.verbose_name
-        else:
-            self.lookup_title = f.verbose_name # use field name
-        rel_name = other_model._meta.pk.name
-        self.lookup_kwarg = '%s__%s__exact' % (self.field_path, rel_name)
-        self.lookup_kwarg_isnull = '%s__isnull' % (self.field_path)
-        self.lookup_val = request.GET.get(self.lookup_kwarg, None)
-        self.lookup_val_isnull = request.GET.get(
-                                      self.lookup_kwarg_isnull, None)
-        if isinstance(f, models.ForeignKey):
-            restrict_to = model.objects.all().values_list(f.attname, flat=True)
-            self.lookup_choices = [(item.pk, str(item)) for item in f.rel.to._default_manager.filter(pk__in=restrict_to)]
-        else:
-            self.lookup_choices = f.get_choices(include_blank=False)
-
-    RelatedFilterSpec.__init__ = new_init
-
     def adminsite_index(self, request, extra_context=None):
         """
         Displays the main admin index page, which lists all of the installed
