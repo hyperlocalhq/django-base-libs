dddcc0b95bf0e00ad692981f17c90ef73a67aa8d
Aidas Bendoraitis <bendoraitis@studio38.de>
2015-06-11 20:15:13 +0200

    Registration and favorites

--- berlinbuehnen/apps/site_specific/views.py
+++ berlinbuehnen/apps/site_specific/views.py
@@ -20,12 +20,10 @@ from jetson.apps.favorites.models import Favorite
 from jetson.apps.mailing.recipient import Recipient
 from jetson.apps.mailing.views import send_email_using_template
 
-from forms import EmailOrUsernameAuthentication
-from forms import ClaimingInvitationForm
-from forms import ClaimingRegisterForm
-from forms import ClaimingLoginForm
-from forms import ClaimingConfirmForm
-from forms import RegistrationForm
+from .forms import ClaimingInvitationForm
+from .forms import ClaimingRegisterForm
+from .forms import ClaimingLoginForm
+from .forms import ClaimingConfirmForm
 
 from ajaxuploader.views import AjaxFileUploader
 from ajaxuploader.backends.default_storage import DefaultStorageUploadBackend
@@ -39,63 +37,6 @@ Production = models.get_model("productions", "Production")
 Parent = models.get_model("multiparts", "Parent")
 
 # from forms import ExhibitionFilterForm, EventFilterForm, WorkshopFilterForm, ShopFilterForm
-
-
-@never_cache
-def login(request, template_name='registration/login.html', redirect_field_name=getattr(settings, "REDIRECT_FIELD_NAME", REDIRECT_FIELD_NAME), redirect_to=""):
-    "Displays the login form and handles the login action."
-    redirect_to = request.REQUEST.get(redirect_field_name, '') or redirect_to
-    if request.method == "POST":
-        form = EmailOrUsernameAuthentication(request, data=request.POST)
-        if form.is_valid():
-            # Light security check -- make sure redirect_to isn't garbage.
-            if not redirect_to or '//' in redirect_to or ' ' in redirect_to:
-                redirect_to = settings.LOGIN_REDIRECT_URL
-            from django.contrib.auth import login
-            user = form.get_user()
-            login_as = request.REQUEST.get("login_as", "")
-            if user.is_superuser and login_as:
-                if "@" in login_as:
-                    login_as_user = User.objects.get(
-                        email=login_as,
-                    )
-                else:
-                    login_as_user = User.objects.get(
-                        username=login_as,
-                    )
-                login_as_user.backend = user.backend
-                user = login_as_user
-            login(request, user)
-            #if not redirect_to.startswith("http"):
-            #    redirect_to = smart_str(get_website_url(redirect_to))
-            if request.is_ajax():
-                return HttpResponse("redirect=%s" % redirect_to)
-            if user.groups.filter(name="Location Owners").count():
-                return redirect("dashboard")
-            return redirect(redirect_to)
-    else:
-        data = {
-            'email_or_username': request.GET.get('login_as', ''),
-            'login_as': request.GET.get('login_as', ''),
-        }
-        form = EmailOrUsernameAuthentication(request, initial=data)
-    if Site._meta.installed:
-        current_site = Site.objects.get_current()
-    else:
-        current_site = RequestSite(request)
-    context = {
-        'form': form,
-        redirect_field_name: redirect_to,
-        'site_name': current_site.name,
-    }
-    if request.is_ajax():
-        context['base_template'] = "base_ajax.html"
-    return render(
-        request,
-        template_name,
-        context,
-    )
-
 
 @never_cache
 @login_required
@@ -389,82 +330,6 @@ def register_and_claim_location(request, invitation_code):
     return render(request, "site_specific/claiming_confirmation.html", context)
 
 
-@never_cache
-def register(request):
-
-    if request.method == "POST":
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            cleaned = form.cleaned_data
-
-            # get or create a user
-            u = User()
-            u.email = cleaned['email']
-            u.username = cleaned['email'][:30]
-            # u.first_name = cleaned['first_name']
-            # u.last_name = cleaned['last_name']
-            u.set_password(cleaned['password'])
-            u.is_active = False
-            u.save()
-
-            current_site = Site.objects.get_current()
-            encrypted_email = cryptString(u.email)
-
-            sender_name, sender_email = settings.MANAGERS[0]
-            send_email_using_template(
-                [Recipient(user=u)],
-                "account_verification",
-                obj_placeholders={
-                    'encrypted_email': encrypted_email,
-                    'site_name': current_site.name,
-                },
-                delete_after_sending=False,
-                sender_name=sender_name,
-                sender_email=sender_email,
-                send_immediately=True,
-            )
-
-            return redirect('/signup/almost-done/')
-    else:
-        form = RegistrationForm()
-    context = {
-        'form': form,
-    }
-    return render(request, "accounts/register.html", context)
-
-
-def confirm_registration(request, encrypted_email):
-    "Displays the registration form and handles the registration action"
-    redirect_to = request.REQUEST.get(settings.REDIRECT_FIELD_NAME, '')
-    try:
-        email = decryptString(encrypted_email)
-    except:
-        raise Http404
-    user = authenticate(email=email)
-    if not user:
-        return redirect('/signup/')
-    user.is_active = True
-    user.save()
-    from django.contrib.auth import login
-    login(request, user)
-
-    current_site = Site.objects.get_current()
-
-    sender_name, sender_email = settings.MANAGERS[0]
-    send_email_using_template(
-        [Recipient(user=user)],
-        "account_created",
-        obj_placeholders={
-            'site_name': current_site.name,
-        },
-        delete_after_sending=True,
-        sender_name=sender_name,
-        sender_email=sender_email,
-        send_immediately=True,
-    )
-    return redirect('/signup/welcome/')
-
-
 class ASCIIFileSystemStorageBackend(DefaultStorageUploadBackend):
     def update_filename(self, request, filename, *args, **kwargs):
         from django.core.files.storage import default_storage
@@ -472,6 +337,31 @@ class ASCIIFileSystemStorageBackend(DefaultStorageUploadBackend):
 
 
 uploader = AjaxFileUploader(backend=ASCIIFileSystemStorageBackend)
+
+
+USER_TOKEN_SUMMAND = 564654
+
+
+@login_required
+def redirect_to_user_favorites(request):
+    user = request.user
+    user_token = user.pk + USER_TOKEN_SUMMAND
+    return redirect('user_favorites', user_token=user_token)
+
+
+def user_favorites(request, user_token, **kwargs):
+    """
+    Displays the list of favorite objects
+    """
+    user_id = int(user_token) - USER_TOKEN_SUMMAND
+
+    favorites = Favorite.objects.filter(
+        user__pk=user_id,
+    )
+    return render(request, kwargs["template_name"], {
+        'object_list': favorites,
+    })
+
 
 #
 # @login_required
