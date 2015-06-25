ecba78245bf0dc1580d62cb2e83bee4f21665e9d
Tiago Henriques <henriques@studio38.de>
2015-06-25 17:59:55 +0200

    use cryptographic hashes from hashlib

--- subtrees/jetson/apps/permissions/templatetags/row_level_permission.py
+++ subtrees/jetson/apps/permissions/templatetags/row_level_permission.py
@@ -1,4 +1,4 @@
-import sha
+from hashlib import sha1
 from django.contrib.contenttypes.models import ContentType
 from django import template
 from django.conf import settings 
@@ -15,7 +15,7 @@ class objref_class(template.Node):
     def render(self, context):
         object_id = template.resolve_variable(self.object_name, context) 
         c_obj = ContentType.objects.get_for_model(object_id).id
-        return "%s/%d/%s" % ( c_obj, object_id.id, sha.new("%s/%d" % (c_obj, object_id.id) + settings.SECRET_KEY).hexdigest())
+        return "%s/%d/%s" % ( c_obj, object_id.id, sha1.new("%s/%d" % (c_obj, object_id.id) + settings.SECRET_KEY).hexdigest())
 
 #Based off work by Ian Holsman
 #http://svn.zyons.python-hosting.com/trunk/zilbo/common/utils/templatetags/media.py
