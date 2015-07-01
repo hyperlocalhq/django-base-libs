a299dfb9b9af8280122f1217eea34945a4e6da1f
Aidas Bendoraitis <bendoraitis@studio38.de>
2015-07-01 17:13:03 +0200

    fixed deprecated code: model._meta.module_name -> model._meta.model_name

--- subtrees/jetson/apps/email_campaigns/admin.py
+++ subtrees/jetson/apps/email_campaigns/admin.py
@@ -124,7 +124,7 @@ class MailingAdmin(ExtendedModelAdmin):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
             return update_wrapper(wrapper, view)
 
-        info = self.model._meta.app_label, self.model._meta.module_name
+        info = self.model._meta.app_label, self.model._meta.model_name
         
         urls = patterns('',
             url(r'^(.+)/send/$',
