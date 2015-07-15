7855311ad284357b21d4cce48cf8729d95ced48a
Tiago Henriques <henriques@studio38.de>
2015-07-15 17:01:48 +0200

    add “replace” flag to mysqldump to avoid duplicate entry problem when importing

--- scripts/mysqldump_data.sh
+++ scripts/mysqldump_data.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 set -eu
 
-mysqldump -u root -p --no-create-info  -R --triggers ccb > ccb_data.sql
+mysqldump -u root -p --replace --no-create-info  -R --triggers ccb > ccb_data.sql
