53833ad4249190d1a8c00408f0ff7f0a3c02d8db
Tiago Henriques <henriques@studio38.de>
2015-08-25 14:23:10 +0200

    add scripts to fix indentation and line endings

---
+++ scripts/cr2lf.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env sh
+find . \
+    -type f \
+    -not -path "./subtrees/*" \
+    -not -path "./.git/*" \
+    -not -path "./media/*" \
+    -not -path "./static/*" \
+    -not -path "./site_static/*" \
+    -not -path "./fixtures/*" \
+    -not -path "*.pyc" \
+    -not -path "*.json" \
+    -not -path "*.sql" \
+    -not -path "*.pdf" \
+    -not -path "*.png" \
+    -not -path "*.jpg" \
+    -not -path "*.gif" \
+    -not -path "*.mo" \
+    -not -path "*.psd" \
+    -not -path "*.ai" \
+    -not -path "./*/.DS_Store" \
+    -exec mac2unix {} \;
