1079890e015365925f7a9b5bbba9aaa829e1d41a
Aidas Bendoraitis <bendoraitis@studio38.de>
2015-09-03 23:09:02 +0200

    List and detail views for educational projects

---
+++ berlinbuehnen/apps/education/urls.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+from django.conf.urls import *
+
+urlpatterns = patterns('berlinbuehnen.apps.education.views',
+    url(r'^$', 'project_list', name='project_list'),
+    # url(r'^add/$', 'add_project', name='add_project'),
+    url(r'^(?P<slug>[^/]+)/$', 'project_detail', name='project_detail'),
+    # url(r'^(?P<slug>[^/]+)/events/$', 'project_events', name='project_events'),
+    # url(r'^(?P<slug>[^/]+)/change/$', 'change_project', name='change_project'),
+    # url(r'^(?P<slug>[^/]+)/delete/$', 'delete_project', name='delete_project'),
+    # url(r'^(?P<slug>[^/]+)/status/$', 'change_project_status', name='change_project_status'),
+    # gallery
+    # url(r'^(?P<slug>[^/]+)/gallery/$', 'image_overview', name='project_image_overview'),
+    # url(r'^(?P<slug>[^/]+)/gallery/add/$', 'create_update_image', name='project_add_image'),
+    # url(r'^(?P<slug>[^/]+)/gallery/file_(?P<mediafile_token>[^/]+)/$', 'create_update_image', name='project_change_image'),
+    # url(r'^(?P<slug>[^/]+)/gallery/file_(?P<mediafile_token>[^/]+)/delete/$', 'delete_image', name='project_delete_image'),
+)
