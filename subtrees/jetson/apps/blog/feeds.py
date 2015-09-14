5ca80211cd83577bef0185cd071388816f900aed
Tiago Henriques <henriques@studio38.de>
2015-09-14 16:17:54 +0200

    fix KeyError on dict lookups

--- subtrees/jetson/apps/blog/feeds.py
+++ subtrees/jetson/apps/blog/feeds.py
@@ -20,7 +20,7 @@ class RssFeed(Feed):
         self.kwargs = kwargs
         
     def title(self):
-        obj = self.kwargs['object']
+        obj = self.kwargs.get('object', None)
         if obj:
             result = _(u"Feeds for '%(obj)s' @ %(site)s") % {
                 'obj': obj,
@@ -36,7 +36,7 @@ class RssFeed(Feed):
         return force_unicode(_("Latest posts"))
 
     def items(self, obj):
-        container = self.kwargs['container']
+        container = self.kwargs.get('container', None)
         return Post.published_objects.filter(blog=container).order_by('-published_from')[:5]
                  
     def item_link(self, obj):
