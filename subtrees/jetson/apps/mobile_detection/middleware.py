81d3a0fb3eb940071fba2a95823db6579232bc35
Tiago Henriques <henriques@studio38.de>
2015-09-17 13:46:56 +0200

    protect against KeyError

--- subtrees/jetson/apps/mobile_detection/middleware.py
+++ subtrees/jetson/apps/mobile_detection/middleware.py
@@ -11,8 +11,8 @@ class MobileDetectionMiddleware(object):
         is_tablet = False
         is_phone = False
 
-        user_agent = request.META.get("HTTP_USER_AGENT")
-        http_accept = request.META.get("HTTP_ACCEPT")
+        user_agent = request.META.get("HTTP_USER_AGENT", "")
+        http_accept = request.META.get("HTTP_ACCEPT", "")
         if user_agent and http_accept:
             agent = mdetect.UAgentInfo(userAgent=user_agent, httpAccept=http_accept)
             is_tablet = agent.detectTierTablet()
