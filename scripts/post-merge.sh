617824242d5eff4b01502612c4699f6055e490d0
Tiago Henriques <tiago.henriques@gmail.com>
2015-09-21 18:47:47 +0200

    convert NewRelic notification script into post-merge hook

--- scripts/post-merge.sh
+++ scripts/post-merge.sh
@@ -1,35 +1,40 @@
 #! /usr/bin/env bash
-# notify NewRelic of new deployment to production
+## MIT © Sindre Sorhus - sindresorhus.com
+
+## git hook to run a command after `git pull` if a specified file was changed
+## Run `chmod +x post-merge` to make it executable then put it into `.git/hooks/`.
+
+## This particular hook notifies NewRelic of a new deployment to the production repo
 
 ## prevent referencing undefined variables (which default to "")
 set -o nounset
 ## ignore failing commands
 set -o errexit
 
-display_usage() {
-    echo "Usage: $0 commit_hash"
-    exit 1
-}
+newrelic_api_key=dfffb2b48abee5204529774c5acef511e512f8a763ea144
+newrelic_application_id=470332
 
-if [ $# -eq 0 ]; then
-    display_usage
-    exit 1
-fi
+commit_description=$(git log -1 --pretty=format:%s)
+commit_author=$(git log -1 --pretty=format:%cn)
+## abbreviated commit SHA:
+commit_revision=$(git log -1 --pretty=format:%h)
+## full commit SHA:
+# commit_revision=$(git log -1 --pretty=format:%H)
 
-# check whether user had supplied -h or --help . If yes display usage
-if [[ ( "$1" == "--help") ||  "$1" == "-h" ]]; then
-    display_usage
-    exit 0
-fi
+## Where to store the log information about the updates
+LOGFILE=./post-merge.log
 
-REVISION=${1}
+##  Record the fact that a merge was performed
+echo -e "Performed merge at $( date +'%Y-%m-%dT%H:%M:%SZ' )" >> ${LOGFILE}
+echo " - New SHA: ${commit_revision}" >> ${LOGFILE}
 
 curl \
--H "x-api-key:dfffb2b48abee5204529774c5acef511e512f8a763ea144" \
--d "deployment[application_id]=470332" \
--d "deployment[description]=New version of the app was deployed" \
--d "deployment[revision]=${REVISION}" \
-https://api.newrelic.com/deployments.xml
+    --header "x-api-key:${newrelic_api_key}" \
+    --data "deployment[application_id]=${newrelic_application_id}" \
+    --data "deployment[description]=${commit_description}" \
+    --data "deployment[user]=${commit_author}" \
+    --data "deployment[revision]=${commit_revision}" \
+    https://api.newrelic.com/deployments.xml
 
-# -d "deployment[changelog]=Changelog" \
-# -d "deployment[user]=User" \
+echo "Notified NewRelic about new deployment" >> ${LOGFILE}
+echo "" >> ${LOGFILE}
