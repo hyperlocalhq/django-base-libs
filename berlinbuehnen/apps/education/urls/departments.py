555509c14dca3a3369272c62e48908bdeab9e155
Aidas Bendoraitis <bendoraitis@studio38.de>
2015-09-24 22:48:55 +0200

    URL rules for education app fixed

--- berlinbuehnen/apps/education/urls/departments.py
+++ berlinbuehnen/apps/education/urls/departments.py
@@ -4,9 +4,10 @@ from django.conf.urls import *
 
 urlpatterns = patterns('berlinbuehnen.apps.education.views.departments',
     url(r'^$', 'department_list', name='department_list'),
+    url(r'^add/$', 'add_department', name='add_department'),
+
     url(r'^(?P<slug>[^/]+)/$', 'department_detail', name='department_detail'),
 
-    url(r'^add/$', 'add_department', name='add_department'),
     url(r'^(?P<slug>[^/]+)/change/$', 'change_department', name='change_department'),
     url(r'^(?P<slug>[^/]+)/delete/$', 'delete_department', name='delete_department'),
     url(r'^(?P<slug>[^/]+)/status/$', 'change_department_status', name='change_department_status'),
--- berlinbuehnen/apps/education/urls/projects.py
+++ berlinbuehnen/apps/education/urls/projects.py
@@ -4,10 +4,11 @@ from django.conf.urls import *
 
 urlpatterns = patterns('berlinbuehnen.apps.education.views.projects',
     url(r'^$', 'project_list', name='project_list'),
+    url(r'^add/$', 'add_project', name='add_project'),
+
     url(r'^(?P<slug>[^/]+)/$', 'project_detail', name='project_detail'),
     url(r'^(?P<slug>[^/]+)/events/(?P<event_id>\d+)/$', 'project_detail', name='project_event_detail'),
 
-    url(r'^add/$', 'add_project', name='add_project'),
     url(r'^(?P<slug>[^/]+)/change/$', 'change_project', name='change_project'),
     url(r'^(?P<slug>[^/]+)/delete/$', 'delete_project', name='delete_project'),
     url(r'^(?P<slug>[^/]+)/delete/$', 'delete_project', name='delete_project'),
