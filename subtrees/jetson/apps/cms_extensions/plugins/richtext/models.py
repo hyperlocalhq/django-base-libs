dbc1dc5a964d6e8702e63a5e98bdf7f63a8e4bb6
Tiago Henriques <tiago.henriques@gmail.com>
2015-10-07 19:41:41 +0100

    fix deprecated import of truncate_words

--- subtrees/jetson/apps/cms_extensions/plugins/richtext/models.py
+++ subtrees/jetson/apps/cms_extensions/plugins/richtext/models.py
@@ -2,12 +2,20 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.html import strip_tags
-from django.utils.text import truncate_words
+from django.utils.text import Truncator
+from django.utils.functional import allow_lazy
+from django.utils import six
 from django.conf import settings
 
 from base_libs.models.fields import ExtendedTextField
 
 from cms.models import CMSPlugin
+
+def truncate_words(s, num, end_text='...'):
+    truncate = end_text and ' %s' % end_text or ''
+    return Truncator(s).words(num, truncate=truncate)
+truncate_words = allow_lazy(truncate_words, six.text_type)
+
 
 class RichText(CMSPlugin):
     """
