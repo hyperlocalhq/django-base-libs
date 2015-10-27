9c3f63a56f46639f3c53e00df66262c6938f9dc5
Tiago Henriques <tiago.henriques@gmail.com>
2015-10-27 18:03:52 +0000

    wrap JSON response in HTML, enabling use of DjDT

--- berlinbuehnen/urls.py
+++ berlinbuehnen/urls.py
@@ -122,3 +122,50 @@ urlpatterns += i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')),
 )
+
+def html_decorator(func):
+    """
+    This decorator wraps the output in html.
+    (From http://stackoverflow.com/a/14647943)
+    """
+    from django.http import HttpResponse
+
+    def _decorated(*args, **kwargs):
+        response = func(*args, **kwargs)
+
+        wrapped = ("<html><body>",
+                   response.content,
+                   "</body></html>")
+
+        return HttpResponse(wrapped)
+
+    return _decorated
+
+
+@html_decorator
+def debug(request):
+    """
+    Debug endpoint that uses the html_decorator,
+    """
+    from django.core import urlresolvers
+    from django.http import HttpResponse
+
+    path = request.META.get("PATH_INFO")
+    api_url = path.replace("debug/", "")
+
+    view = urlresolvers.resolve(api_url)
+
+    accept = request.META.get("HTTP_ACCEPT")
+    accept += ",application/json"
+    request.META["HTTP_ACCEPT"] = accept
+
+    res = view.func(request, **view.kwargs)
+    return HttpResponse(res._container)
+
+
+# And finally attach debug to urls (if settings is on)
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^debug/', debug),
+    )
+
