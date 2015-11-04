94e5302d60e680eece1d67857d5a9d3ca607e598
Tiago Henriques <tiago.henriques@gmail.com>
2015-11-04 18:43:09 +0000

    ignore all non-django cookies

--- berlinbuehnen/middleware.py
+++ berlinbuehnen/middleware.py
@@ -1,11 +1,23 @@
+## code taken from "Fixing Site-Wide Caching in Django":
+## https://www.silviogutierrez.com/blog/fixing-site-wide-caching-django/
+
+## update code to ignore all non-django cookies, using example from:
+## http://www.stavros.io/posts/djangos-site-caching-doesnt-work/
+
 from django.middleware.cache import UpdateCacheMiddleware
 
 import re
 
-
 class SmartUpdateCacheMiddleware(UpdateCacheMiddleware):
-    STRIP_RE = re.compile(r'\b(_[^=]+=.+?(?:; |$))')
-
     def process_request(self, request):
-        cookie = self.STRIP_RE.sub('', request.META.get('HTTP_COOKIE', ''))
-        request.META['HTTP_COOKIE'] = cookie
+        cookies = request.META.get('HTTP_COOKIE', '')
+
+        # Strip all non-Django cookies.
+        new_cookies = []
+        for cookie in re.split("\;\s*", cookies):
+            key, value = cookie.split("=")
+            if "=" not in cookie:
+                continue
+            if key.lower().strip() in ("csrftoken", "sessionid"):
+                new_cookies.append("%s=%s" % (key, value))
+        request.META['HTTP_COOKIE'] = "; ".join(new_cookies)
