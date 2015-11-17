bce081c7244af41a4a1711cc1373a74be4245955
Aidas Bendoraitis <bendoraitis@studio38.de>
2015-11-17 18:51:56 +0100

    crispy form for notification settings

--- subtrees/jetson/apps/notification/views.py
+++ subtrees/jetson/apps/notification/views.py
@@ -1,6 +1,5 @@
 # -*- coding: UTF-8 -*-
-from django.core.urlresolvers import reverse
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.http import HttpResponseRedirect, Http404
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
@@ -14,6 +13,9 @@ from jetson.apps.notification.tasks import get_notification_setting
 from jetson.apps.notification.decorators import basic_auth_required, simple_basic_auth_callback
 from jetson.apps.notification.feeds import NoticeUserFeed
 
+from .forms import NoticeSettingsForm
+
+
 @basic_auth_required(realm='Notices Feed', callback_func=simple_basic_auth_callback)
 def feed_for_user(request):
     url = "feed/%s" % request.user.username
@@ -22,6 +24,7 @@ def feed_for_user(request):
         "feed": NoticeUserFeed(),
     })
     return feed
+
 
 @login_required
 @never_cache
@@ -36,23 +39,25 @@ def notices(request):
             setting = get_notification_setting(request.user, notice_type, medium_id)
             settings_row.append((form_sysname, setting.frequency))
         settings_table.append({"notice_type": notice_type, "cells": settings_row})
-    
+
     notice_settings = {
         "column_headers": [medium_display for medium_id, medium_display in NOTICE_MEDIA],
         "rows": settings_table,
     }
-    
+
     return render_to_response("notification/notices.html", {
         "notices": notices,
         "notice_types": notice_types,
         "notice_settings": notice_settings,
     }, context_instance=RequestContext(request))
 
+
 class NoticeSettingForm(forms.Form):
     frequency = forms.ChoiceField(
         label=_("How often should the email be sent?"),
         choices=NOTICE_FREQUENCY,
-        )
+    )
+
 
 @login_required
 @never_cache
@@ -60,6 +65,7 @@ def notification_settings(request):
     notice_types = NoticeType.objects.all()
     notices = Notice.objects.notices_for(request.user)
     settings_table = []
+
     if request.user.is_staff:
         queryset = NoticeType.objects.all()
     else:
@@ -73,7 +79,7 @@ def notification_settings(request):
                 form = NoticeSettingForm(
                     prefix=form_sysname,
                     data=request.POST,
-                    )
+                )
                 if form.is_valid():
                     setting.frequency = form.cleaned_data['frequency']
                 setting.save()
@@ -81,24 +87,35 @@ def notification_settings(request):
                 form = NoticeSettingForm(
                     prefix=form_sysname,
                     initial={'frequency': setting.frequency},
-                    )
+                )
             settings_row.append((form_sysname, form))
         settings_table.append({
             "category": notice_type.category,
             "notice_type": notice_type,
             "cells": settings_row,
-            })
-    
+        })
+
     notice_settings = {
         "column_headers": [medium_display for medium_id, medium_display in NOTICE_MEDIA],
         "rows": settings_table,
     }
-    
+
+    # TODO: uncomment when ready
+    # if request.method == "POST":
+    #     form = NoticeSettingsForm(request.user, data=request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect(".")
+    # else:
+    #     form = NoticeSettingsForm(request.user)
+
     return render_to_response("notification/settings.html", {
+        # "form": form, # TODO: uncomment when ready
         "notices": notices,
         "notice_types": notice_types,
         "notice_settings": notice_settings,
     }, context_instance=RequestContext(request))
+
 
 @login_required
 def single(request, id):
@@ -109,6 +126,7 @@ def single(request, id):
         }, context_instance=RequestContext(request))
     raise Http404
 
+
 @login_required
 def archive(request, noticeid=None, next_page=None):
     if noticeid:
@@ -116,12 +134,13 @@ def archive(request, noticeid=None, next_page=None):
             notice = Notice.objects.get(id=noticeid)
             if request.user == notice.user or request.user.is_superuser:
                 notice.archive()
-            else:   # you can archive other users' notices
-                    # only if you are superuser.
+            else:  # you can archive other users' notices
+                # only if you are superuser.
                 return HttpResponseRedirect(next_page)
         except Notice.DoesNotExist:
             return HttpResponseRedirect(next_page)
     return HttpResponseRedirect(next_page)
+
 
 @login_required
 def delete(request, noticeid=None, next_page=None):
@@ -130,12 +149,13 @@ def delete(request, noticeid=None, next_page=None):
             notice = Notice.objects.get(id=noticeid)
             if request.user == notice.user or request.user.is_superuser:
                 notice.delete()
-            else:   # you can delete other users' notices
-                    # only if you are superuser.
+            else:  # you can delete other users' notices
+                # only if you are superuser.
                 return HttpResponseRedirect(next_page)
         except Notice.DoesNotExist:
             return HttpResponseRedirect(next_page)
     return HttpResponseRedirect(next_page)
+
 
 @login_required
 def mark_all_seen(request):
