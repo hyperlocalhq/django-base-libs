5b5becc3354dc1dadd24dd1f0a19a4e3660f0bca
Daniel Lehmann <dlehmann@dreammedia.info>
2015-12-01 02:51:09 +0100

    style guide update

---
+++ site_static/site/js/form.js
@@ -0,0 +1,58 @@
+/**
+ * Handles form functionality and styling.
+ *
+ * @author Daniel Lehmann
+ */
+
+$(document).ready(function() {
+    
+    
+    // the input prefix highlighting
+    $('.input-field input[type="text"]').each(function() {
+        
+        var $field = $(this);
+        var $prefix = $field.parent().prev('.input-prefix');
+        
+        if ($prefix.length) {
+         
+            $field.data('$prefix', $prefix);
+            
+            var onFocus = function() {
+                var $field = $(this);
+                var css_class = ($field.prop('readonly')) ? 'focus-readonly' : 'focus';
+                $field.data('$prefix').addClass(css_class);
+            }
+            
+            var onBlur = function() {
+                var $field = $(this);
+                $field.data('$prefix').removeClass('focus-readonly focus');
+            }
+            
+            $field.focus(onFocus);
+            $field.blur(onBlur);
+        }
+    });
+    
+    
+    // auto size of textareas
+    $('.input-field textarea').each(function() {
+        
+        var $field = $(this);
+        
+        var onChange = function($field, delay) {
+            
+            if (typeof delay != "undefined") {
+                setTimeout(function() {var $me=$field; onChange($me);}, 0);
+            }
+            
+            $field.css('height', '');
+            $field.height($field.get(0).scrollHeight - 30);            
+        }
+        
+        $field.change(function() {onChange($(this));});
+        $field.on('cut paste drop keydown', function() {onChange($(this), true);});
+        
+        onChange($field);
+    });
+    
+});
