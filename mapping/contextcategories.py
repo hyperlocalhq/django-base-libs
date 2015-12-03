41c5cb7af5586498219fbb49c5f8f2ecc163a47c
Tiago Henriques <tiago.henriques@gmail.com>
2015-12-03 13:31:49 +0100

    map all category sysnames to their titles

---
+++ mapping/contextcategories.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+#  -*- coding: UTF-8 -*-
+
+import csv
+from pprint import pprint as pp
+
+d = None
+
+with open('tsv/contextcategories.tsv') as f:
+    r = csv.DictReader(f, delimiter='\t')
+    ls = [(
+              row['sysname'],
+              row['title'],
+          ) for row in r]
+    d = dict(ls)
+
+pp(d)
