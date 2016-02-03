a00f6ea2aa6a34e416d6c8eca5c8080d44d68106
Aidas Bendoraitis <bendoraitis@studio38.de>
2016-02-03 18:33:24 +0100

    Expire cache for the page to redirect to when a blog post is created

--- base_libs/utils/cache.py
+++ base_libs/utils/cache.py
@@ -1,11 +1,11 @@
 # -*- coding: UTF-8 -*-
 from django.core.cache import cache
-from django.http import HttpRequest
 from django.utils.cache import get_cache_key
 
 
 def expire_page(request, path):
     request.path = path
+    request.method = "GET"
     key = get_cache_key(request)
     if cache.has_key(key):
         cache.delete(key)
