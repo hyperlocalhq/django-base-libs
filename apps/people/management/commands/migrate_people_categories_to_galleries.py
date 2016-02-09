a7a81368e5c2decc5d935143896a6a898aaed75d
Tiago Henriques <trinosauro@gmail.com>
2016-02-09 14:34:31 +0100

    Tweak migration script

--- apps/people/management/commands/migrate_people_categories_to_galleries.py
+++ apps/people/management/commands/migrate_people_categories_to_galleries.py
@@ -3,12 +3,9 @@ from django.core.management.base import NoArgsCommand
 from django.db import transaction
 from django.db.utils import IntegrityError
 from django.contrib.contenttypes.models import ContentType
-import csv
 
 from ccb.apps.media_gallery.models import MediaGallery
 from ccb.apps.people.models import Person
-from ccb.apps.site_specific.models import ContextItem
-from jetson.apps.structure.models import Category
 
 
 class Command(NoArgsCommand):
@@ -26,16 +23,16 @@ class Command(NoArgsCommand):
                     )
                     categories = person.categories.filter(level=0)
                     for gallery in galleries:
-                        print '\tmigrating person categories to gallery: {}'.format(gallery)
+                        print '\tmigrating person categories to gallery "{}"'.format(gallery)
                         for category in categories:
-                            print '\t\tadding category {} with level {} to gallery {}'.format(
+                            print '\t\tadding category "{}" with level {} to gallery "{}"'.format(
                                 category, category.level, gallery
                             )
                             gallery.categories.add(category)
                             # ContextItem.objects.update_for(gallery)
             except IntegrityError:
-                print '** integrity error migrating categories for person {} **'.format(
-                    person.id,
+                print '** integrity error migrating categories for person "{}" with id {} **'.format(
+                    person.user.username, person.id,
                 )
                 problems.add((person.id))
         if problems:
