aa7bcd3c019da27249126dc4116b1f950c3b6b8b
Aidas Bendoraitis <bendoraitis@studio38.de>
2016-03-09 21:16:19 +0100

    raven for sentry installed

--- settings.py
+++ settings.py
@@ -179,6 +179,7 @@ INSTALLED_APPS = (
     "haystack",
     "ajaxuploader",
     "debug_toolbar",
+    "raven.contrib.django.raven_compat",
 
     ### Required CMS Django 2.4.1 apps ###
     "cms",
@@ -821,6 +822,61 @@ MAILING_CONTENT_TYPE_CHOICES = (
 ROSETTA_STORAGE_CLASS = "rosetta.storage.SessionRosettaStorage"
 
 USE_TZ = True
+
+### SENTRY ###
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s '
+                      '%(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+            'tags': {'custom-tag': 'x'},
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
+
+import raven
+
+RAVEN_CONFIG = {
+    'dsn': 'http://ac6037beb199460a9fe190c75dac8074:8fc8ca56432b4ac28e5670b80ab3e426@sentry.jetsonproject.org/5',
+    # If you are using git, you can also automatically configure the
+    # release based on the git info.
+    'release': raven.fetch_git_sha(os.path.dirname(__file__)),
+}
 
 ### LOCAL SETTINGS ###
 
