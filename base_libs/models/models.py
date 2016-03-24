088d627530e694df15a5bd0b54ffc7b883e46020
Aidas Bendoraitis <bendoraitis@studio38.de>
2016-03-25 00:56:22 +0100

    Blog fix (need to be tested on the production)

--- base_libs/models/models.py
+++ base_libs/models/models.py
@@ -182,7 +182,7 @@ class PublishingMixinPublishedManager(models.Manager):
             published_till=None,
             ))
         conditions.append(models.Q(
-            published_from__lt=now,
+            published_from__lte=now,
             published_till=None,
             ))
         conditions.append(models.Q(
@@ -190,7 +190,7 @@ class PublishingMixinPublishedManager(models.Manager):
             published_till__gt=now,
             ))
         conditions.append(models.Q(
-            published_from__lt=now,
+            published_from__lte=now,
             published_till__gt=now,
             ))
         return super(
