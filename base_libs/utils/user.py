e5e92afe02ecd00656282421542181b137458d67
Aidas Bendoraitis <bendoraitis@studio38.de>
2016-04-05 16:07:06 +0200

    Upgrade to Django 1.9.5 and Django CMS 3.2.3 (not yet fully tested)

--- base_libs/utils/user.py
+++ base_libs/utils/user.py
@@ -4,5 +4,7 @@ from django.utils.encoding import force_unicode
 
 def get_user_title(user):
     """ Returns user's first and last name or username or nickname """
-    return force_unicode(user.profile)
-
+    try:
+        return force_unicode(user.profile)
+    except:
+        return force_unicode(user)
