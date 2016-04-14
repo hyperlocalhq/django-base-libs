4db85d7538facad7f4dea452b2e587bddec451e1
Tiago Henriques <trinosauro@gmail.com>
2016-04-14 17:02:16 +0200

    Improve documentation for notification tests

--- tests/notification_tests.py
+++ tests/notification_tests.py
@@ -1,19 +1,52 @@
 # -*- coding: UTF-8 -*-
 '''
-This script helps automate the creation of instances of the models that should generate email notifications. In order to use, you need to make sure that:
-- Redis is running:
-    $ redis-server
-- Celery is running:
-    $ ./manage.py celeryd --beat --loglevel=INFO
-- MailHog is running:
-    $ MailHog
+This script helps automate the creation of random instances of the models that should generate email notifications.
 
+In order to use it, please keep in mind the following requirements:
+- AutoFixture should be installed:
+    $ pip install django-autofixture
+- AutoFixture should be enabled:
+    INSTALLED_APPS should include 'autofixture'
+- Redis should be running:
+    $ redis-server
+- MailHog should be running:
+    $ MailHog
+- Celery should be running:
+    $ ./manage.py celeryd --beat --loglevel=INFO
+- Optionally, if you want to monitor pending tasks, run the Celery Camera:
+    $ ./manage.py celery events --camera=djcelery.snapshot.Camera
+
+On FreeBSD, both Celery processes are managed by supervisorctl. Here are some example commands (must be root):
+    # supervisorctl status
+    celery_beat                      STOPPED    Apr 14 04:58 PM
+    celery_camera                    STOPPED    Apr 14 04:58 PM
+
+    # supervisorctl start all
+    celery_camera: started
+    celery_beat: started
+
+    # supervisorctl status
+    celery_beat                      RUNNING    pid 39455, uptime 0:00:33
+    celery_camera                    RUNNING    pid 39456, uptime 0:00:33
+
+    # supervisorctl restart all
+    celery_camera: stopped
+    celery_beat: stopped
+    celery_beat: started
+    celery_camera: started
+
+    # supervisorctl tail celery_beat
+    ...
+
+    # supervisorctl tail celery_beat stderr
+    ...
+    
 Remember that for MailHog intercept sent emails, you need to configure Django for it. Add the following to your local_settings.py:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
 
-If go and MailHog are not installed, install go. On FreeBSD, it's simply:
+If go and MailHog are not installed, first install go. On FreeBSD, it's simply:
     $ pkg install go
 and on Mac it's
     $ brew install go
@@ -21,14 +54,12 @@ Once go is installed, install MailHog:
     $ go get github.com/mailhog/MailHog
 Remember to set $GOPATH, and to add $GOPATH/bin to your PATH.
 
-Optionally, if you want to monitor pending tasks, run the Celery Camera:
-    $ ./manage.py celery events --camera=djcelery.snapshot.Camera
-
 To check whether object creation results in sending emails, open the MailHog web console, available by default at http://localhost:8025.
 '''
 
 from autofixture import AutoFixture
 
+from ccb.apps.people.models import Person
 from ccb.apps.events.models import Event
 from ccb.apps.marketplace.models import JobOffer
 from ccb.apps.bulletin_board.models import Bulletin
