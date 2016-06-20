18d678e18f32b37469a5a766a0f80cd148481418
Aidas Bendoraitis <bendoraitis@studio38.de>
2016-06-20 18:40:36 +0200

    museumssommer updated

--- settings.py
+++ settings.py
@@ -609,7 +609,6 @@ CMS_TEMPLATES = [
     ('cms/magazine_special_event.html', gettext(u'Magazine – Special Event')),
 
     ('cms/museumssummer_first.html', gettext(u'Museums Summer – First')),
-    ('cms/museumssummer_first_with_ad.html', gettext(u'Museums Summer – First with ad')),
     ('cms/museumssummer_second.html', gettext(u'Museums Summer – Second')),
     ('cms/museumssummer_second_featured.html', gettext(u'Museums Summer – Second - Featured')),
     ('cms/museumssummer_third.html', gettext(u'Museums Summer - Third')),
