45a3f00cfab84d57c3d210a0b72957e486ee93cc
Daniel Lehmann <dlehmann@dreammedia.info>
2016-08-11 11:31:39 +0200

    added festivals and educational departments to location detail pages

--- berlinbuehnen/apps/productions/templatetags/production_tags.py
+++ berlinbuehnen/apps/productions/templatetags/production_tags.py
@@ -7,31 +7,32 @@ from urllib import urlencode
 from filebrowser.models import FileDescription
 from django.utils.translation import ugettext as _
 
+
 register = template.Library()
 
 @register.inclusion_tag('productions/includes/other_productions.html', takes_context=True)
 def other_productions(context, current_event=False, current_location=False, amount=24):
     from berlinbuehnen.apps.productions.models import Production
-    
+
     if current_location:
         locations = [current_location]
     elif current_event:
         locations = list(current_event.production.in_program_of.all()) # + list(current_event.ev_or_prod_play_locations())
     else:
         locations = []
-        
+
     timestamp = tz_now()
     other_production_set = Production.objects.filter(
         models.Q(in_program_of__in=locations) | models.Q(play_locations__in=locations),
         show_among_others=True,
         status="published",
     ).exclude(start_date__lt=timestamp.date()).order_by('start_date', 'start_time').distinct()
-    
+
     if current_event:
         other_production_set = other_production_set.exclude(id=current_event.production.id)
-        
+
     other_production_set = other_production_set[:amount]
-        
+
     return {
         'current_event': current_event,
         'current_location': current_location,
@@ -43,17 +44,17 @@ def other_productions(context, current_event=False, current_location=False, amou
 
 @register.inclusion_tag('productions/includes/date_slider.html', takes_context=True)
 def date_slider(context, page, active=0, id=''):
-    
+
     today = datetime.today()
 
     days = []
     for i in range(0, 7):
         days += [date(2015, 4, 5 + i)]
-    
+
     months = []
     for i in range(1, 13):
         months += [date(2015, i, 1)]
-    
+
     return {
         'today': today,
         'active': active,
@@ -63,8 +64,8 @@ def date_slider(context, page, active=0, id=''):
         'page': page,
         'STATIC_URL': context['STATIC_URL'],
     }
-    
-    
+
+
 @register.inclusion_tag('productions/includes/bvg.html', takes_context=True)
 def bvg(context, address="", address_2=False, postal_code=False, city=False, event_date=False, event_time=False, delta=0, timesel="depart"):
 
@@ -81,7 +82,7 @@ def bvg(context, address="", address_2=False, postal_code=False, city=False, eve
         to += city
 
     today = datetime.now()
-    
+
     if event_date and event_time:
         event_datetime = datetime(year=event_date.year, month=event_date.month, day=event_date.day, hour=event_time.hour, minute=event_time.minute)
         if (event_datetime < today):
@@ -89,12 +90,12 @@ def bvg(context, address="", address_2=False, postal_code=False, city=False, eve
             event_time = False
             delta = 0
             timesel = "depart"
-    
+
     if event_date:
         today = datetime(year=event_date.year, month=event_date.month, day=event_date.day, hour=today.hour, minute=today.minute)
     if event_time:
         today = datetime(year=today.year, month=today.month, day=today.day, hour=event_time.hour, minute=event_time.minute)
-    
+
     if delta < 0:
         delta *= -1;
         today -= timedelta(minutes=delta)
@@ -131,7 +132,7 @@ def pin(context, image, description=""):
         'description': description
     }
     href = "https://www.pinterest.com/pin/create/button/?"+urlencode(param)
-        
+
     return {
         'href': href,
         'protected': protected
@@ -150,7 +151,7 @@ def add_to_calender(context, event):
     end_time = event.end_time
 
     two_hours = timedelta(hours=2)
-    
+
     if start_datetime and not end_time:
         end_datetime = start_datetime + two_hours
 
@@ -162,7 +163,7 @@ def add_to_calender(context, event):
     if description:
         description += "\n\n"
     description += context['request'].build_absolute_uri(event.get_url_path())
-        
+
     return {
         'event': event,
         'end_date': end_date,
