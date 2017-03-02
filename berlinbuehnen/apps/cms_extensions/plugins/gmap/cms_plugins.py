1201cdad48f722950fda7be2c13fc435bd97a99a
Aidas Bendoraitis <bendoraitis@studio38.de>
2017-03-02 19:56:24 +0100

    GOOGLE_API_KEY added to all maps

--- berlinbuehnen/apps/cms_extensions/plugins/gmap/cms_plugins.py
+++ berlinbuehnen/apps/cms_extensions/plugins/gmap/cms_plugins.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.utils.translation import ugettext_lazy as _
 from django.forms.widgets import Media
+from django.conf import settings
 
 from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
 
-from museumsportal.apps.cms_extensions.plugins.gmap.models import GMap
+from .models import GMap
 
 class GMapPlugin(CMSPluginBase):
+    # TODO: Do we still need this plugin? If YES, get_plugin_media should be replaced with sekizai. If NOT, it should be uninstalled and removed
     model = GMap
     name = _("Google Map V3")
     render_template = "cms/plugins/gmap.html"
@@ -20,6 +22,8 @@ class GMapPlugin(CMSPluginBase):
         return context
     
     def get_plugin_media(self, request, context, plugin):
-        return Media(js = ('http://maps.google.com/maps/api/js?sensor=false',))
+        return Media(
+            js=('//maps.google.com/maps/api/js?key={}'.format(getattr(settings, "GOOGLE_API_KEY", "")),)
+        )
  
 plugin_pool.register_plugin(GMapPlugin)
