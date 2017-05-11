b7ed75bfa6f321b5e0f8fc44042b6379522c6e95
Aidas Bendoraitis <bendoraitis@studio38.de>
2017-05-11 21:03:56 +0200

    CKEditor is using an open-source oembed widget instead of Embed with iframe.ly

--- site_static/bower_components/ckeditor/config.js
+++ site_static/bower_components/ckeditor/config.js
@@ -7,46 +7,30 @@ CKEDITOR.config.forcePasteAsPlainText = false;
 
  
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here.
-	// For complete reference see:
-	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+    // Define changes to default configuration here.
+    // For complete reference see:
+    // http://docs.ckeditor.com/#!/api/CKEDITOR.config
 
-	// The toolbar groups arrangement, optimized for two toolbar rows.
-	/*config.toolbarGroups = [
-		{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		{ name: 'links' },
-		{ name: 'insert' },
-		{ name: 'forms' },
-		{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'others' },
-		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'about' }
-	];*/
+    // Set the most common block elements.
+    config.format_tags = 'p;h2;h4;h5';
 
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	//config.removeButtons = 'Underline,Subscript,Superscript';
-
-	// Set the most common block elements.
-	config.format_tags = 'p;h2;h4;h5';
-
-	// Simplify the dialog windows.
-	//config.removeDialogTabs = 'image:advanced;link:advanced';
-	config.removeDialogTabs = 'link:advanced';
+    // Simplify the dialog windows.
+    //config.removeDialogTabs = 'image:advanced;link:advanced';
+    config.removeDialogTabs = 'link:advanced';
     
-    config.extraPlugins = 'embed';
-    
+    config.extraPlugins = 'oembed,widget';
+    config.oembed_maxWidth = '560';
+    config.oembed_maxHeight = '315';
+    config.oembed_WrapperClass = 'embededContent';
+    config.allowedContent = true;
+
     config.toolbar = [
-        ["Cut", "Copy", "-", "Paste", "PasteText", "PasteFromWord", "-", "Undo", "Redo"], ["Scayt"], ["Styles"], ["Source"], ["Maximize"],
+        ['Cut', 'Copy', '-', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo'], ['Scayt'], ['Styles'], ['Source'], ['Maximize'],
         '/',
-        ["Bold", "Italic", "-", "Underline", "Strike", "-", "Subscript", "Superscript", "-", "RemoveFormat"], ["Image", "-", "Embed", "-", "SpecialChar", "-", "HorizontalRule", "-", "Blockquote"], ["Link", "Unlink", "Anchor"]
-    ]
+        ['Bold', 'Italic', '-', 'Underline', 'Strike', '-', 'Subscript', 'Superscript', '-', 'RemoveFormat'], ['Image', '-', 'oembed', '-', 'SpecialChar', '-', 'HorizontalRule', '-', 'Blockquote'], ['Link', 'Unlink', 'Anchor']
+    ];
 };
 
-document.write("<link href='https://fonts.googleapis.com/css?family=Lato:400,400italic,700,700italic,300,300italic,900' rel='stylesheet' type='text/css'>");
+if (window.jQuery) {
+    jQuery('head').append('<link href="https://fonts.googleapis.com/css?family=Lato:400,400italic,700,700italic,300,300italic,900" rel="stylesheet" type="text/css">');
+}
