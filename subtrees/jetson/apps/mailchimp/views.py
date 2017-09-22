7793c80e944c9d20a95674ef5a510de0fdef6f47
Aidas Bendoraitis <aidasbend@yahoo.com>
2017-09-22 05:02:51 +0200

    ✨ Simplified newsletter subscription (mailing list selected by the current language, no fields for user name)

--- subtrees/jetson/apps/mailchimp/views.py
+++ subtrees/jetson/apps/mailchimp/views.py
@@ -7,7 +7,8 @@ from django.conf import settings
 from django.views.decorators.cache import never_cache
 from django.utils.translation import ugettext as _
 
-from jetson.apps.mailchimp.forms import SubscriptionForm
+from jetson.apps.mailchimp.forms import SubscriptionForm, SimpleSubscriptionForm
+
 
 @never_cache
 def subscribe_for_info(request, *arguments, **keywords):
@@ -24,6 +25,30 @@ def subscribe_for_info(request, *arguments, **keywords):
             form.fields['last_name'].initial = getattr(request.user, "last_name", "")
             form.fields['email'].initial = getattr(request.user, "email", "")
     return render_to_response('mailchimp/subscription.html', {
+        'form': form,
+        'site_name': Site.objects.get_current().name,
+    }, context_instance=RequestContext(request))
+
+
+@never_cache
+def subscribe_for_info_simplified(request, *arguments, **keywords):
+    """Displays the info subscription form and handles the subscription action"""
+    if request.method == "POST":
+        form = SimpleSubscriptionForm(request.LANGUAGE_CODE, request.POST)
+        if form.is_valid():
+            result = form.save(request)
+            if result['status'] == 400:
+                if result['title'] == 'Member Exists':
+                    form.add_error('email', _("You have already subscribed to the newsletter with this email."))
+                else:
+                    form.add_error('email', _("Unknown error. Try to subscribe with another email or try to subscribe to this newsletter later."))
+            else:
+                return redirect('subscribe_for_info_simplified_done')
+    else:
+        form = SimpleSubscriptionForm(request.LANGUAGE_CODE)
+        if request.user.is_authenticated():
+            form.fields['email'].initial = getattr(request.user, "email", "")
+    return render_to_response('mailchimp/subscription_simplified.html', {
         'form': form,
         'site_name': Site.objects.get_current().name,
     }, context_instance=RequestContext(request))
