7d13f9d2c06320c60a44d81edf6606c3b55c08be
Aidas Bendoraitis <aidasbend@yahoo.com>
2017-10-05 08:14:25 +0200

    🔨 Photo author for events added to administration

--- apps/events/models.py
+++ apps/events/models.py
@@ -41,7 +41,7 @@ class Event(ComplexEventBase):
         blank=True
     )
 
-    photo_author = models.CharField(_("Photo"), max_length=100, blank=True)
+    photo_author = models.CharField(_("Photo author"), max_length=100, blank=True)
 
     objects = ExtendedEventManager()
 
