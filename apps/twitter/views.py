4c314ba571c516877d27864a7a686b6ab5a2721d
Aidas Bendoraitis <bendoraitis@studio38.de>
2017-11-22 20:48:48 +0100

    👾 tweet list fixed

--- apps/twitter/views.py
+++ apps/twitter/views.py
@@ -2,7 +2,7 @@ import re
 import json
 
 from dateutil.parser import parse as datetime_parse
-from twython import Twython
+from twython import Twython, TwythonRateLimitError, TwythonAuthError
 from django.conf import settings
 from django.http import HttpResponse
 from django.utils.html import urlize
@@ -10,8 +10,6 @@ from django.utils import dateformat
 from django.views.decorators.cache import cache_page
 from django.template.defaultfilters import timesince
 from django.utils.translation import ugettext
-
-
 
 
 @cache_page(60 * 15)
@@ -22,12 +20,17 @@ def latest_tweets(request, twitter_username, number_of_tweets):
         settings.TWITTER_ACCESS_TOKEN,
         settings.TWITTER_ACCESS_TOKEN_SECRET,
     )
-    tweets = twitter.get_user_timeline(
-        include_entities="true",
-        include_rts="true",
-        screen_name=twitter_username,
-        count=number_of_tweets,
-    )
+    try:
+        tweets = twitter.get_user_timeline(
+            include_entities="true",
+            include_rts="true",
+            screen_name=twitter_username,
+            count=number_of_tweets,
+        )
+    except (TwythonRateLimitError, TwythonAuthError):
+        # if there is a rate-limit or authentication error, return empty list
+        return HttpResponse(json.dumps([]), content_type="application/json")
+
     for tweet in tweets:
         tweet['text_urlized'] = urlize(tweet['text'])
         tmp = []
