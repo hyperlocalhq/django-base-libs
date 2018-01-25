7be23a5e8f7f450645ffd681a07683a585f3af97
Aidas Bendoraitis <bendoraitis@studio38.de>
2018-01-25 21:59:59 +0100

    🔨 Unified markup options for all extended text fields

--- apps/editorial/cms_plugins.py
+++ apps/editorial/cms_plugins.py
@@ -1,26 +1,19 @@
 # -*- coding: utf-8 -*-
-import re
-
 from django.utils.translation import ugettext as _
 
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 
+from base_libs.admin.options import MarkupTypeOptions
+
 from .models import QuestionAnswer, Document
 
 
-class QuestionAnswerPlugin(CMSPluginBase):
+class QuestionAnswerPlugin(MarkupTypeOptions, CMSPluginBase):
     model = QuestionAnswer
     name = _("Question and Answer")
     render_template = "cms/plugins/question_answer.html"
     change_form_template = "cms/plugins/question_answer_plugin_change_form.html"
-
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        # add .markupType to body_markup_type
-        field = super(QuestionAnswerPlugin, self).formfield_for_dbfield(db_field, **kwargs)
-        if re.search('markup_type$', db_field.name):
-            field.widget.attrs['class'] = "markupType"
-        return field
 
     def render(self, context, instance, placeholder):
         context.update({
@@ -33,20 +26,13 @@ class QuestionAnswerPlugin(CMSPluginBase):
 plugin_pool.register_plugin(QuestionAnswerPlugin)
 
 
-class DocumentPlugin(CMSPluginBase):
+class DocumentPlugin(MarkupTypeOptions, CMSPluginBase):
     model = Document
     name = _("Document")
     render_template = "cms/plugins/document.html"
     change_form_template = "cms/plugins/document_plugin_change_form.html"
 
     filter_horizontal = ["categories"]
-
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        # add .markupType to body_markup_type
-        field = super(DocumentPlugin, self).formfield_for_dbfield(db_field, **kwargs)
-        if re.search('markup_type$', db_field.name):
-            field.widget.attrs['class'] = "markupType"
-        return field
 
     def render(self, context, instance, placeholder):
         context.update({
