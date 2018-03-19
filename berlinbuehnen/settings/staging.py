c6985cb1bb446d2376d6874d6de862315b044fa9
Aidas Bendoraitis <bendoraitis@studio38.de>
2018-03-19 22:33:25 +0100

    ⚙️ MySQL is not an alternative in production and staging environments

--- berlinbuehnen/settings/staging.py
+++ berlinbuehnen/settings/staging.py
@@ -12,27 +12,8 @@ DATABASES = {
         'USER': get_secret("DATABASE_USER"),
         'PASSWORD': get_secret("DATABASE_PASSWORD"),
     },
-    'mysql': {  # TODO: this is here for historical reason. To be removed.
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': get_secret("MYSQL_DATABASE_NAME"),
-        'USER': get_secret("MYSQL_DATABASE_USER"),
-        'PASSWORD': get_secret("MYSQL_DATABASE_PASSWORD"),
-        'CONN_MAX_AGE': 0,
-        'HOST': 'localhost',
-        'OPTIONS': {
-            'init_command': 'SET default_storage_engine=MYISAM,'
-                'character_set_connection=utf8,'
-                'collation_connection=utf8_general_ci,'
-                'foreign_key_checks=0'
-            # 'init_command': 'SET storage_engine=INNODB,'
-            #     'character_set_connection=utf8,'
-            #     'collation_connection=utf8_general_ci,'
-            #     'foreign_key_checks=0'
-        },
-    }
 }
 DATABASES['default'] = DATABASES['postgresql']
-del DATABASES['mysql']
 
 PREPEND_WWW = False
 
