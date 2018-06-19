3e4697deb149d5432be1b176d2e35ad1d892d494
Aidas Bendoraitis <bendoraitis@studio38.de>
2018-06-19 21:46:02 +0200

    ⚙️ importing from Kulturmanagement made verbose

--- deployment/production/commands/import_jobs_from_kulturmanagement.sh
+++ deployment/production/commands/import_jobs_from_kulturmanagement.sh
@@ -9,7 +9,7 @@ date >> ${CRON_LOG_FILE}
 cd ${PROJECT_PATH}
 source bin/activate
 cd project/ccb    
-python manage.py import_jobs_from_kulturmanagement --traceback --settings=settings.production >> ${CRON_LOG_FILE}  2>&1
+python manage.py import_jobs_from_kulturmanagement --verbosity=2 --traceback --settings=settings.production >> ${CRON_LOG_FILE}  2>&1
 
 echo "Finished." >> ${CRON_LOG_FILE}
 duration=$SECONDS
