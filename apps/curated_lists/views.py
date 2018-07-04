5656e75a8e546070d9c53b8886ffe446abc8db82
Aidas Bendoraitis <bendoraitis@studio38.de>
2018-07-04 23:36:42 +0200

    🚧 Editing curated lists (in progress)

--- apps/curated_lists/views.py
+++ apps/curated_lists/views.py
@@ -1,8 +1,9 @@
 # -*- coding: UTF-8 -*-
 from django.contrib.contenttypes.models import ContentType
 from hashids import Hashids
+from django.db import models
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, Http404
+from django.http import Http404, JsonResponse
 
 from jetson.apps.utils.decorators import login_required
 from jetson.apps.utils.views import object_list
@@ -117,3 +118,124 @@ def featured_curated_lists(request, **kwargs):
     extra_context['facets'] = facets
 
     return object_list(request, **kwargs)
+
+
+@login_required
+def user_curated_lists_json(request):
+    """
+    Returns a list of curated lists created by the currently logged in user or their institutions
+
+    If content_type_id and object_id are passed as query parameters,
+    also checks each list if an item with that content_type_id and object_id exists in that list.
+
+    :param request:
+    :return: Example: [
+        {
+            "owner": {"id": 123, "title": "Joe Smith", "type": "person"}
+            "lists": [
+                {"token": "sdfgbyx", "title": "Best Companies", "item_included": false}, 
+                {"token": "nbyasf", "title": "Best Galleries", "item_included": true}]
+        },
+        {
+            "owner": {"id": 124, "title": "Joe & Co", "type": "institution"}
+            "lists": [
+                {"token": "sdfbsdf", "title": "Geniuses", "item_included": false},
+                {"token": "dfbddss", "title": "Best Galleries", "item_included": false}]
+        },
+        {
+            "owner": {"id": 124, "title": "The Smiths", "type": "institution"}
+            "lists": [
+                {"token": "bjdfsms", "title": "Favorites", "item_included": false}
+            ]
+        },
+    ]
+    """
+    item_content_type = item_object_id = None
+    if request.GET:
+        item_content_type = ContentType.objects.get(pk=request.GET.get('content_type_id'))
+        item_object_id = request.GET.get('object_id')
+    data = []
+    data_item = {}
+    person = request.user.profile
+    data_item['owner'] = {
+        'id': person.pk,
+        'title': person.get_title(),
+        'type': 'people.person',
+    }
+    curated_lists = CuratedList.objects.filter(
+        listowner__owner_content_type=ContentType.objects.get_for_model(person),
+        listowner__owner_object_id=person.pk,
+    )
+    if item_content_type and item_object_id:
+        curated_lists = curated_lists.annotate(
+            item_included=models.Sum(
+                models.Case(
+                    models.When(
+                        listitem__content_type=item_content_type,
+                        listitem__object_id=item_object_id,
+                        then=1,
+                    ),
+                    default=0,
+                    output_field=models.IntegerField()
+                )
+            )
+        )
+    data_item['lists'] = [{
+        'token': curated_list.get_token(),
+        'title': curated_list.title,
+        'item_included': bool(getattr(curated_list, "item_included", False)),
+    } for curated_list in curated_lists]
+    data.append(data_item)
+
+    for contact in person.individualcontact_set.exclude(institution=None).only("institution"):
+        data_item = {}
+        institution = contact.institution
+        curated_lists = CuratedList.objects.filter(
+            listowner__owner_content_type=ContentType.objects.get_for_model(institution),
+            listowner__owner_object_id=institution.pk,
+        )
+        if item_content_type and item_object_id:
+            curated_lists = curated_lists.annotate(
+                item_included=models.Sum(
+                    models.Case(
+                        models.When(
+                            listitem__content_type=item_content_type,
+                            listitem__object_id=item_object_id,
+                            then=1,
+                        ),
+                        default=0,
+                        output_field=models.IntegerField()
+                    )
+                )
+            )
+        data_item['owner'] = {
+            'id': institution.pk,
+            'title': institution.get_title(),
+            'type': 'institutions.institution',
+        }
+        data_item['lists'] = [{
+            'token': curated_list.get_token(),
+            'title': curated_list.title,
+            'item_included': bool(getattr(curated_list, "item_included", False)),
+        } for curated_list in curated_lists]
+        data.append(data_item)
+
+    return JsonResponse(data, safe=False)
+
+
+@login_required
+def add_user_curated_list_json(request):
+    data = {}
+    return JsonResponse(data)
+
+
+@login_required
+def change_user_curated_list_json(request, token):
+    data = {}
+    return JsonResponse(data)
+
+
+@login_required
+def delete_user_curated_list_json(request, token):
+    data = {}
+    return JsonResponse(data)
