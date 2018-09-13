028648286be3c7b6ae4414a2e337ba91d3b62981
Aidas Bendoraitis <bendoraitis@studio38.de>
2018-09-13 18:05:26 +0200

    Squashed 'subtrees/base_libs/' changes from 359bf6a0b7..d8ae7536fb
    d8ae7536fb 👾 All functional tests are made passing
    5ddd4c3a58 Merge commit '9a393c299d84d4baba83f2b89b0e521a60c4bebb'
    52bb846d6d 🔨 base_libs refactored: base_tags moved to base_libs.apps.base_libs_app.templatetags
    36e53683e1 🚧 Security policy as one single line
    61257cdd1b 👾 Mailing app fixed with cases when users have no profiles
    650f1353a3 👾 URLizing of rendered extended text fields uses bleach.linkify() instead of Django's default urlize()
    9125272fdc 🔨 Migration to PostgreSQL. 👾 Fixes for PostgreSQL. 👾 Permission managing for CMS pages fixed.
    
    git-subtree-dir: subtrees/base_libs
    git-subtree-split: d8ae7536fb017819d75c0925c5c7f37763d0521c

--- base_libs/utils/user.py
+++ base_libs/utils/user.py
@@ -4,5 +4,9 @@ from django.utils.encoding import force_unicode
 
 def get_user_title(user):
     """ Returns user's first and last name or username or nickname """
-    return force_unicode(user.profile)
+    profile = getattr(user, "profile", None)
+    if profile:
+        return force_unicode(profile)
+    return "{} {}".format(user.first_name, user.last_name).strip() or user.username
+
 
