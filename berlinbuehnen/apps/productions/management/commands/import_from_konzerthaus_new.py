1ff977716e22ef51f39d5987eff8005cc52bc487
Tiago Henriques <henriques@studio38.de>
2018-09-20 18:20:36 +0200

    🔨 Improve Konzerthaus import

--- berlinbuehnen/apps/productions/management/commands/import_from_konzerthaus_new.py
+++ berlinbuehnen/apps/productions/management/commands/import_from_konzerthaus_new.py
@@ -5,6 +5,8 @@ from ._import_to_berlinbuehnen_base_xml import ImportToBerlinBuehnenBaseXML
 class Command(ImportToBerlinBuehnenBaseXML):
     help = "Imports productions and events from Konzerthaus"
 
+    IMPORT_URL = "https://www.konzerthaus.de/berlin_buehnen.xml"
+
     def prepare(self):
         from django.db import models
         Service = models.get_model("external_services", "Service")
@@ -12,7 +14,11 @@ class Command(ImportToBerlinBuehnenBaseXML):
         self.service, created = Service.objects.get_or_create(
             sysname="bb_konzerthaus_prods",
             defaults={
-                'url': "http://www.konzerthaus.de/berlin_buehnen.xml",
+                'url': self.IMPORT_URL,
                 'title': u"BB Konzerthaus Productions",
             },
         )
+
+        if self.service.url != self.IMPORT_URL:
+            self.service.url = self.IMPORT_URL
+            self.service.save()
