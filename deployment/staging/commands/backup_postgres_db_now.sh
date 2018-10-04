9ad265e545aedd3a103c50e7dbb4a64cb0c35059
Tiago Henriques <henriques@studio38.de>
2018-10-04 12:08:29 +0200

    Fix DB user and DB name

--- deployment/staging/commands/backup_postgres_db_now.sh
+++ deployment/staging/commands/backup_postgres_db_now.sh
@@ -4,8 +4,8 @@ PROJECT_PATH=/usr/local/www/apache24/data/ccb.jetsonproject.org
 CRON_LOG_FILE=${PROJECT_PATH}/logs/backup_postgres_db_now.log
 DATE_TIMESTAMP=$(LC_ALL=en_US.UTF-8 date +"%Y%m%d-%H%M")
 BACKUP_PATH=${PROJECT_PATH}/db_backups/${DATE_TIMESTAMP}.backup
-USER=creativeberlin
-DATABASE=creativeberlin
+USER=jp_ccb
+DATABASE=jp_ccb
 
 cd ${PROJECT_PATH}
 mkdir -p logs
