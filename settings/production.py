23a032823b88bdfba8caa6b9d71a453af3795af2
Aidas Bendoraitis <bendoraitis@studio38.de>
2019-02-21 17:46:35 +0100

    ⚙️ MySQL dependency removed from production.

--- settings/production.py
+++ settings/production.py
@@ -12,18 +12,6 @@ DATABASES = {
         'USER': get_secret("DATABASE_USER"),
         'PASSWORD': get_secret("DATABASE_PASSWORD"),
     },
-    'mysql': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': get_secret("DATABASE_NAME"),
-        'USER': get_secret("DATABASE_USER"),
-        'PASSWORD': get_secret("DATABASE_PASSWORD"),
-        'OPTIONS': {
-            'init_command': 'SET storage_engine=INNODB,'
-                'character_set_connection=utf8,'
-                'collation_connection=utf8_general_ci,'
-                'foreign_key_checks=0'
-        },
-    }
 }
 DATABASES['default'] = DATABASES['postgresql']
 HUEY['name'] = DATABASES['default']['NAME']
